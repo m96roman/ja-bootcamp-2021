fa16295845a37769786bc078281e9e26f6d4e067
Yevhen Shyptur <eugeni.shyptur@gmail.com>
2021-09-27 17:17:32 +0300

    update

---
+++ Bootcamp/Lecture1/Homework/shyptur/myfirstproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myfirstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
