2987485d0dc3e8d0e518dc4cc4fc469c7fb205fa
Martin Roshko <martin.roshko1@gmail.com>
2021-09-27 18:35:23 +0300

    Homework

---
+++ Bootcamp/HomeWork1/Task2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = 3;
+            int [] inputValue= new int[countOfNumbers];
+
+            for (int i =0; i < countOfNumbers ;i++)
+            {
+                
+                inputValue[i] = int.Parse(Console.ReadLine());
+            }
+
+            multiplication(inputValue);
+
+        }
+
+        static void multiplication(int [] arr ) {
+            int length = arr.Length;
+            int result = 1;
+            for (int i = 0;i < length; i++) {
+                result *= arr[i];
+            }
+            Console.WriteLine(result.ToString()) ;
+        }
+    }
+}
---
+++ Bootcamp/Task5/Task5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Task5
+{
+    class Task5
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = 3;
+            int[] inputValue = new int[countOfNumbers];
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+
+                inputValue[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(inputValue);
+
+            Console.WriteLine($"Largest value = {inputValue[inputValue.Length-1].ToString()}\nLowest value = {inputValue[0].ToString()}");
+
+        }
+    }
+}
