c02eff8942cf6884a3b86008a62c8fb3130e97ad
Yuriy Plyska <Yuriyply@gmail.com>
2021-09-27 20:45:20 +0300

    first commit test

---
+++ Bootcamp/IuriiPlyskaLec1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IuriiPlyskaLec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void 
+    }
+}
