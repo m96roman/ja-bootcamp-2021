ea62811e710df6b07e7578fbee9afac50d56e8f1
tennesseee <antdvgi@gmail.com>
2021-09-28 10:07:28 +0300

    Homework

---
+++ Bootcamp/Lecture1/Homework/AntonDvgHomework/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+
+namespace AntonDvgHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task12();
+        }
+
+        static void Task1()
+        {
+            int value = 20;
+            Console.WriteLine($"{nameof(value)} = {value}");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int thirdNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(firstNum * secondNum * thirdNum);
+
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            char first = str[0];
+            char last = str[str.Length - 1];
+            char[] strArr = str.ToCharArray();
+            strArr[0] = last;
+            strArr[strArr.Length - 1] = first;
+            string result = "";
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                result += strArr[i];
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = firstNum + secondNum;
+
+            if (firstNum == secondNum)
+            {
+                Console.WriteLine(sum * 3);
+            }
+            else
+            {
+                Console.WriteLine(sum / 8);
+            }
+
+        }
+        static void Task5()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Largest value = {Math.Max(firstNum, Math.Max(secondNum, thirdNum))}");
+            Console.WriteLine($"Lowest value = {Math.Min(firstNum, Math.Min(secondNum, thirdNum))}");
+        }
+        static void Task6()
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            int length = str.Length;
+            if (length < 5)
+            {
+                return;
+            }
+            string res = "";
+            for (int i = 0; i < length; i++)
+            {
+                if (i < 5)
+                {
+                    res += str[i].ToString().ToUpper();
+                }
+                else
+                {
+                    res += str[i];
+                }
+
+            }
+            Console.WriteLine(res);
+        }
+
+        static void Task7()
+        {
+            Console.WriteLine("Enter an integer: ");
+            int someInt = int.Parse(Console.ReadLine());
+            Console.WriteLine(someInt % 3 == 0 || someInt % 7 == 0);
+        }
+
+        static void Task8()
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            char[] strArr = str.ToCharArray();
+            if (strArr[0] != 'C' && strArr[1] != '#')
+            {
+                Console.WriteLine(false);
+            }
+            else
+            {
+                Console.WriteLine("Given string starts with C#");
+            }
+
+        }
+        static void Task9()
+        {
+            Console.WriteLine("Enter the radius of the circle: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"Perimeter of the circle = {perimeter}");
+            Console.WriteLine($"Area of the circle = {area}");
+        }
+
+        static void Task10()
+        {
+            double sum = 0;
+            int length = 10;
+            Console.WriteLine($"Enter {length} numbers from keyboard");
+            for (int i = 0; i < length; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+
+            }
+            double avg = sum / length;
+
+            Console.WriteLine($"Sum = {sum}, Average = {avg}");
+        }
+
+        static void Task11()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    Console.Write($"{i}x{j}={i * j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Task12()
+        {
+            int numA = int.Parse(Console.ReadLine());
+            int numB = int.Parse(Console.ReadLine());
+            int counter = 0;
+            if (numA < numB)
+            {
+                for (int i = numA; i < numB; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                        counter++;
+                        i += 7;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Range is invalid");
+            }
+            Console.WriteLine($"Numbers in range diviseble by 8: { counter }");
+
+        }
+
+        static void Task13()
+        {
+            string str = "";
+            Console.WriteLine("enter number of chanel:");
+            int chanel = int.Parse(Console.ReadLine());
+
+            switch ((TvChanelsEnum)chanel)
+            {
+                case TvChanelsEnum.ChanelOne:
+                    str = "Chanel One rating is 85%";
+                    break;
+                case TvChanelsEnum.ChanelTwo:
+                    str = "Chanel Two rating is 90%";
+                    break;
+                case TvChanelsEnum.ChanelThree:
+                    str = "Chanel Three rating is 95%";
+                    break;
+                case TvChanelsEnum.ChanelFour:
+                    str = "Chanel Four rating is 70%";
+                    break;
+                case TvChanelsEnum.ChanelFive:
+                    str = "Chanel Five rating is 80%";
+                    break;
+                default:
+                    str = "There is not such chanel";
+                    break;
+            }
+
+            Console.WriteLine(str);
+        }
+        enum TvChanelsEnum
+        {
+            ChanelOne = 1,
+            ChanelTwo = 2,
+            ChanelThree = 3,
+            ChanelFour = 4,
+            ChanelFive = 5
+        }
+    }
+}
