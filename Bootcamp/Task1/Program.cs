979e74803f96d7da10c02951e7fa727cd49ecb0e
kateryna.fedak <katefedakk.gmail.com>
2021-09-28 10:14:33 +0300

    Console.WriteLine(bool) overload so  i dont need ? operator

--- Bootcamp/Task1/Program.cs
+++ Bootcamp/Task1/Program.cs
@@ -6,45 +6,54 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Print("1");
-            NameOfAndInterpolation();
+            //Print("1");
+            //NameOfAndInterpolation();
 
-            Print("2");
-            Console.WriteLine("Write three numbers trought ,");
-            var numbers = Array.ConvertAll(Console.ReadLine().Split(","), Double.Parse);
-            Console.WriteLine($" The result of multiplication {Multiplication(numbers)}");
+            //Print("2");
+            //Console.WriteLine("Write three numbers trought ,");
+            //var numbers = Array.ConvertAll(Console.ReadLine().Split(","), Double.Parse);
+            //Console.WriteLine($" The result of multiplication {Multiplication(numbers)}");
 
-            Print("3");
-            Console.WriteLine("Write word");
-            var word = Console.ReadLine();
-            Console.WriteLine($"Swap letters in word :{word} => {Swap(word)}");
-            Print("4");
-            Console.WriteLine("Write two integers trought ,");
-            var numbersToSum = Array.ConvertAll(Console.ReadLine().Split(","), decimal.Parse);
-            SumOfNumbersIfEqual(numbersToSum);
-            Print("5");
-            Console.WriteLine("Write three integers trought ,");
-            var numbersMaxAndMin = Array.ConvertAll(Console.ReadLine().Split(","), Int32.Parse);
-            MaxAndMinInt(numbersMaxAndMin);
-            Print("6");
-            Console.WriteLine("Write a string");
-            FiveUpperChar(Console.ReadLine());
+            //Print("3");
+            //Console.WriteLine("Write word");
+            //var word = Console.ReadLine();
+            //Console.WriteLine($"Swap letters in word :{word} => {Swap(word)}");
+          
+            //Print("4");
+            //Console.WriteLine("Write two integers trought ,");
+            //var numbersToSum = Array.ConvertAll(Console.ReadLine().Split(","), decimal.Parse);
+            //SumOfNumbersIfEqual(numbersToSum);
+            
+            //Print("5");
+            //Console.WriteLine("Write three integers trought ,");
+            //var numbersMaxAndMin = Array.ConvertAll(Console.ReadLine().Split(","), Int32.Parse);
+            //MaxAndMinInt(numbersMaxAndMin);
+           
+            //Print("6");
+            //Console.WriteLine("Write a string");
+            //FiveUpperChar(Console.ReadLine());
+           
             Print("7");
             Console.WriteLine("Write a number");
             Multiple3Or7(Int32.Parse(Console.ReadLine()));
+          
             Print("8");
             Console.WriteLine("Write a string");
             StartWithCSharp(Console.ReadLine());
+          
             Print("9");
             Console.WriteLine("Input the radius of the circle : ");
             CirclePerimeterAndArea(Double.Parse(Console.ReadLine()));
+          
             Print("10");
             Console.WriteLine("Write 10 integers trought space");
             var numbersSumAndAvg = Array.ConvertAll(Console.ReadLine().Split(" "), Int32.Parse);
             SumAndAvg(numbersSumAndAvg);
+          
             Print("11");
             Console.WriteLine("Write a program to display multiplication table from 1 to n \n Write n :");
             MultiplicationTable(Int32.Parse(Console.ReadLine()));
+          
             Print("12");
             Console.WriteLine("Write two integers trought space");
             var numbersDivideBy8 = Array.ConvertAll(Console.ReadLine().Split(" "), Int32.Parse);
@@ -62,6 +71,7 @@ namespace Task1
             Console.WriteLine($"Variable {nameof(number)} and it`s value  {number}");
             Console.WriteLine($"Variable {nameof(word)} and it`s value {word}");
         }
+
         public static double Multiplication(double[] numbers)
         {
             double result = 1;
@@ -71,11 +81,13 @@ namespace Task1
             }
             return result;
         }
+
         public static string Swap(string str)
         {
             var length = str.Length;
             return str[length - 1] + str.Substring(1, length - 2) + str[0];
         }
+
         public static void SumOfNumbersIfEqual(decimal[] numbers)
         {
             var sum = numbers[0] + numbers[1];
@@ -100,7 +112,7 @@ namespace Task1
         }
         public static void Multiple3Or7(int number)
         {
-            Console.WriteLine(number % 3 == 0 || number % 7 == 0 ? "true" : "false");
+            Console.WriteLine(number % 3 == 0 || number % 7 == 0);
         }
         public static void StartWithCSharp(string str)
         {
