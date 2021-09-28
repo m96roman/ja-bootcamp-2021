84d647eef97425758420a7d94a7e9bf7a49c7aef
Yuriy Plyska <Yuriyply@gmail.com>
2021-09-28 10:52:11 +0300

    Update old methods and added the last 4 tasks

--- Bootcamp/YuriyPlLect1/Program.cs
+++ Bootcamp/YuriyPlLect1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -29,37 +30,32 @@ namespace YuriyPlLect1
         /// </summary>
         static void MultipleNumbers()
         {
-            Console.WriteLine("Please input first number");
-            var firstNumber = Console.ReadLine().Trim();
-            int firstValue;
-
-           if (!int.TryParse(firstNumber, out firstValue))
-           {
-                Console.WriteLine("Please input only integer. Try again.");
-                return;
-           }
-
-            Console.WriteLine("Please input second number");
-            var secondNumber = Console.ReadLine().Trim();
-            int secondValue;
-
-            if (!int.TryParse(secondNumber, out secondValue))
+            int firstValue = ReadNumber();
+            int secondValue = ReadNumber();
+            int thirdValue = ReadNumber();
+            if (firstValue == -1 || secondValue == -1 || thirdValue == -1)
             {
-                Console.WriteLine("Please input only integer. Try again.");
-                return;
-            }
-
-            Console.WriteLine("Please input third number");
-            var thirdNumber = Console.ReadLine().Trim();
-            int thirdValue;
-
-            if (!int.TryParse(thirdNumber, out thirdValue))
-            {
-                Console.WriteLine("Please input only integer. Try again.");
+                Console.WriteLine("Try again!");
                 return;
             }
 
             Console.WriteLine("The result is"+ firstValue * secondValue * thirdValue);
+
+            static int ReadNumber()
+            {
+                Console.WriteLine("Please input  number");
+                var firstNumber = Console.ReadLine().Trim();
+                int firstValue;
+
+                if (!int.TryParse(firstNumber, out firstValue))
+                {
+                    Console.WriteLine("Please input only integer.");
+                    return -1;
+                }
+
+                return firstValue;
+            }
+
         }
 
         /// <summary>
@@ -105,7 +101,7 @@ namespace YuriyPlLect1
                 return;
             }
 
-            var result = firstValue == secondValue ? firstValue + secondValue : (firstValue + secondValue) / 8;
+            double result = firstValue == secondValue ? firstValue + secondValue : (firstValue + secondValue) / 8;
 
             Console.WriteLine(result);
         }
@@ -143,7 +139,9 @@ namespace YuriyPlLect1
                 Console.WriteLine(text);
             }
 
-            Console.WriteLine(new string(text.Take(5).ToArray()).ToUpper() + new string(text.Skip(5).ToArray()).ToString());
+            //Console.WriteLine(new string(text.Take(5).ToArray()).ToUpper() + new string(text.Skip(5).ToArray()).ToString());
+            string result = string.Join("", text.Take(5)).ToUpper() + string.Join("", text.Skip(5));
+            Console.WriteLine(result);
         }
 
         /// <summary>
@@ -168,7 +166,7 @@ namespace YuriyPlLect1
                 Console.WriteLine("Value can not be an empty");
                 return false;
             }
-            return value.Trim().Take(2).ToArray().ToString() == "C#";
+            return value.StartsWith("C#");
         }
 
         /// <summary>
@@ -191,9 +189,132 @@ namespace YuriyPlLect1
                 return;
             }
 
-            Console.WriteLine("Area of circle is" + 3.14 * radius * radius + "and Perimeter is" + 2 * 2.14 * radius);
+            Console.WriteLine("Area of circle is" + Math.PI * radius * radius + "and Perimeter is" + 2 * Math.PI * radius);
             
         }
 
+        /// <summary>
+        /// Find  sum and average of 10 numbers
+        /// </summary>
+        static void FindAvg()
+        {
+            List<int> container = new List<int>();
+            var counter = 1;
+            while (counter != 10)
+            {
+                int number;
+                if (!ReadNumber(out number))
+                {
+                    Console.WriteLine("Please input only integer. Try agin!");
+                    return;
+                }
+                container.Add(number);
+                counter++;
+            }
+
+            Console.WriteLine("Sum of ten numbers is" + container.Sum() + "Average is" + (double)container.Sum() / 10);
+            static bool ReadNumber( out int value)
+            {
+                Console.WriteLine("Please input integer number");
+                
+                if (!int.TryParse(Console.ReadLine(), out value))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+
+        }
+
+        /// <summary>
+        /// display multiplication table
+        /// </summary>
+        /// <param name="number"></param>
+        static void PresentMultiplicationTable(int number)
+        {
+            if (number <= 0)
+            {
+                Console.WriteLine("number has to be more than 0");
+                return;
+            }
+            int counter = 1;
+            while (counter != number)
+            {
+                for (int x = 1; x <= number; x++)
+                {
+                    Console.Write($"{counter}X{x}=" + counter * x);
+                }
+                Console.WriteLine();
+                counter++;
+            }
+        }
+
+        /// <summary>
+        /// Print all numbers in range between first and second number that are divisible by 8
+        /// If range is invalid return null
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        static IEnumerable<int> DivisibleByEight(int firstNumber, int secondNumber)
+        {
+            var container = new List<int>();
+            if ((secondNumber - firstNumber) <= 0)
+            {
+                Console.WriteLine("Range is not valid. Please input first number less than second");
+                return null; 
+            }
+
+            for (int x = firstNumber; x <= secondNumber; x++)
+            {
+                if (x % 8 == 0)
+                {
+                    container.Add(x);
+                }
+            }
+
+            return container;
+
+        }
+
+        /// <summary>
+        /// provides channel avarage rating
+        /// </summary>
+        /// <param name="tvChannl"></param>
+        static void AvgRaring(TV tvChannl)
+        {
+            switch (tvChannl)
+            {
+                case TV.Sky:
+                    Console.WriteLine($"Rating is for {nameof(TV.Sky)} " + TV.Sky);
+                    break;
+                case TV.HBO:
+                    Console.WriteLine($"Rating is for {nameof(TV.HBO)} " + TV.HBO);
+                    break;
+                case TV.Netflix:
+                    Console.WriteLine($"Rating is for {nameof(TV.Netflix)} " + TV.Netflix);
+                    break;
+                case TV.AppleTv:
+                    Console.WriteLine($"Rating is for {nameof(TV.AppleTv)} " + TV.AppleTv);
+                    break;
+                case TV.SkySport:
+                    Console.WriteLine($"Rating is for {nameof(TV.SkySport)} " + TV.SkySport);
+                    break;
+                default:
+                    Console.WriteLine("Can not find the channel");
+                    break;
+            }
+        }
+
     }
+    enum TV 
+    { 
+        Sky = 30,
+        HBO = 38,
+        Netflix = 49,
+        AppleTv = 59,
+        SkySport
+    }
+
 }
