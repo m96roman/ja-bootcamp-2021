c5d7a1b3eeb01401eff90ab8698cdfd98ebc3bf7
Nestor Pylat <nestor.pylat@justanswer.com>
2021-09-28 19:03:46 +0300

    add 6th task

---
+++ Bootcamp/Lecture2/Tasks/NestorPylatLecture2/NestorPylatLecture2/7_Task.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace NestorPylatLecture2
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+        public Point(int X, int Y)
+        {
+            int nArray = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[nArray];
+
+            for (int i = 0; i < nArray; i++)
+            {
+                try
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                    String.Join(",", array);
+                    Console.WriteLine(array);
+                }
+                catch
+                {
+                    Console.WriteLine("not a Number");
+                }
+                {
+
+                }
+            }
+
+            x = X;
+            y = Y;
+
+        }
+        public void Func()
+
+        {
+            Console.WriteLine()
+        
+    } }
+
+    class _7_Task
+    {
+
+        public static void doTask7()
+        {
+
+        }
+    }
+}
--- Bootcamp/Lecture2/Tasks/NestorPylatLecture2/NestorPylatLecture2/Program.cs
+++ Bootcamp/Lecture2/Tasks/NestorPylatLecture2/NestorPylatLecture2/Program.cs
@@ -11,11 +11,12 @@ namespace NestorPylatLecture2
         static void Main(string[] args)
         {
             //_1_Task.doTask();
-            _2_Task.doTask2();
+            // _2_Task.doTask2();
             //_3_Task.doTask3();
             //_4_Task.doTask4();
             //_5_Task.doTask5();
-
+            //_6_Task.doTask6();
+            //_7_Task.doTask7();
 
 
 
