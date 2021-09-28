2e927c004cd3695ee7adddf432c10efe08d9ea03
tennesseee <antdvgi@gmail.com>
2021-09-28 23:57:12 +0300

    ADvg lecture2

---
+++ Bootcamp/Lecture2/Tasks/ADovhanychHW/Arrays.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public static class Arrays
+{
+    public static void ReversArr()
+    {
+        int i, n;
+        int[] arr = new int[100];
+
+        Console.WriteLine("Enter number of elements in arr:");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Input {n} elements in arr");
+
+        for (i = 0; i < n; i++)
+        {
+            Console.Write($"element - {i} :");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"The values store into the array are :");
+
+        for (i = 0; i < n; i++)
+        {
+            Console.Write($"{arr[i]}");
+        }
+
+        Console.WriteLine("\nThe values store into the array in reverse are : ");
+
+        for (i = n - 1; i >= 0; i--)
+        {
+            Console.Write($"{arr[i]}");
+        }
+
+    }
+
+    public static void Duplicate()
+    {
+        int i, n, j;
+        int[] arr = new int[100];
+        int counter = 0;
+
+        Console.WriteLine("Enter number of elements in arr:");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Input {n} elements in arr");
+
+        for (i = 0; i < n; i++)
+        {
+            Console.Write($"element - {i} :");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    counter++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Total number of duplicate elements found in the array is: {counter}");
+        Console.ReadLine();
+    }
+}
---
+++ Bootcamp/Lecture2/Tasks/ADovhanychHW/ValueVsRef.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class ValueVsRef
+{
+    public static void MarkerTask()
+    {
+        Marker Marker1;
+        Marker Marker2;
+
+        Marker1.color = "red";
+        Marker.index = 2;
+        Marker2.color = "black";
+        Marker.index = 4;
+
+        Marker1.PrintMarker();
+        Marker2.PrintMarker();
+    }
+
+    public struct Marker
+    {
+        public string color;
+        public static int index;
+
+        public Marker(string color)
+        {
+            this.color = color;
+        }
+
+        public void PrintMarker()
+        {
+            Console.WriteLine($"Color of the marker is {color}, index: {index}");
+        }
+
+        public static Marker? InitMarker(string color)
+        {
+            if(color == null)
+            {
+                return new Marker?();
+            }
+
+            return new Marker(color);
+        }
+        
+        public static void DisplayInitMark(string color)
+        {
+            Marker marker = InitMarker(color) ?? new Marker("default");
+            marker.PrintMarker();
+        }
+
+    }
+
+    public static void BookInfo()
+    {
+        Books Book1;
+        Books Book2;
+
+        Console.WriteLine("Information of the first book");
+        Console.Write("Input name of the first book: ");
+        Book1.Title = Console.ReadLine();
+        Console.Write("Input author of the first book:");
+        Book1.Author = Console.ReadLine();
+
+        Console.WriteLine("Information of the second book");
+        Console.Write("Input name of the second book:");
+        Book2.Title = Console.ReadLine();
+        Console.Write("Input author of the second book:");
+        Book2.Author = Console.ReadLine();
+
+        Console.WriteLine($"1: Title = {Book1.Title},  Author = {Book1.Author}");
+        Console.WriteLine($"2: Title = {Book2.Title},  Author = {Book2.Author}");
+
+
+    }
+
+    struct Books
+    {
+        public string Title;
+        public string Author;
+    }
+}
