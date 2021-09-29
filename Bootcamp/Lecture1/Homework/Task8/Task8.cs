b99d0e711263b66deddb1a8200799c6a05bc61c1
roshmar1 <91473204+roshmar1@users.noreply.github.com>
2021-09-29 15:50:06 +0300

    MRoshko Task 2 (#20)
    * Homework
    
    * Homework
    
    * Homework1
    
    * Homework
    
    * Methods done
    
    * All done
    
    * Homework2
    
    * Minor bug fixes
    
    * Removed regions and fixed method names
    Replaced project in specified folder
    Fixed point issue(Nullable<Point>)
    
    * update
    
    * VHerasymenko - Task 1 (#5)
    
    * First Lecture Home Work
    
    * Resolve comments
    
    Co-authored-by: volodymyr herasymenko <volodymyr.herasymenko@justanswer.com>
    
    * add intial proj task 3 (#24)
    
    Co-authored-by: oleh.krutii <oleh.krutii@justanswer.com>
    
    * upDate Homework
    
    * fix the solution
    
    * Add my project
    
    * Update
    
    * clean up
    
    Co-authored-by: Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
    Co-authored-by: Dmytro <dmutroivd@gmail.com>
    Co-authored-by: Volodymyr Herasymenko <vovagerasimenko.zet@gmail.com>
    Co-authored-by: volodymyr herasymenko <volodymyr.herasymenko@justanswer.com>
    Co-authored-by: torbochka8 <olegkrutiy@gmail.com>
    Co-authored-by: oleh.krutii <oleh.krutii@justanswer.com>
    Co-authored-by: DmytrIvd <73742017+DmytrIvd@users.noreply.github.com>

---
+++ Bootcamp/Lecture1/Homework/Task8/Task8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task8
+{
+    class Task8
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+
+            
+                Console.WriteLine((inputText[0] == 'C' && inputText[1] == '#' ? true : false).ToString());
+            
+        }
+    }
+}
