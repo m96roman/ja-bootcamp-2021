4ddf485a2a1cd450090cb1b95aef8b2d4ae4ccc3
Mykhailo Roman <mykhailo.roman@justanswer.com>
2021-09-29 11:23:28 +0300

    added new project

---
+++ Bootcamp/Lecture3/Homework/MykhailoR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MykhailoR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
