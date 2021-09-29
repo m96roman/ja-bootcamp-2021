0ba3740df1d7d3de0762c243cbf846c031ee9836
Viktoriia Dedenok <afrovika@gmail.com>
2021-09-29 16:59:26 +0300

    updatedTask2

--- Bootcamp/Lecture2/Tasks/VDedenok-task2/VDedenok-task2.cs
+++ Bootcamp/Lecture2/Tasks/VDedenok-task2/VDedenok-task2.cs
@@ -6,6 +6,28 @@ namespace TasksLecture2
     {
         static void Main(string[] args)
         {
+            //1.Write a program to create a function to swap the values of two integer numbers.  
+            // Solution of this task I added
+
+            Console.WriteLine("Enter first number:");
+            int num11 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int num12 = int.Parse(Console.ReadLine());
+
+
+            ChangePlaces(ref num11, ref num12);
+            Console.WriteLine($"Nom first number is: {num11}, second number is {num12}.");
+
+
+            static void ChangePlaces(ref int num1, ref int num2)
+            {
+                int serveWord = num1;
+                num1 = num2;
+                num2 = serveWord;
+
+            }
+
 
             //Array, 1. Write a program to read n number of values in an array and display it in reverse order.
             string[] numbers = new string[3];
@@ -25,8 +47,8 @@ namespace TasksLecture2
             Console.WriteLine($"The values store into the array in reverse are : {reversed}");
 
 
-
             //Array, 2.Write a program  to count a total number of duplicate elements in an array.
+
             int[] dublicates = new int[3];
 
             Console.WriteLine("Enter first number:");
@@ -51,7 +73,7 @@ namespace TasksLecture2
             Console.WriteLine($"Total number of duplicate elements found in the array is : {counter}");
 
 
-           
+
             //3.Write a program to insert the information of two books.
             Console.WriteLine("Enter the title of the book:");
             string title1 = Console.ReadLine();
@@ -76,26 +98,19 @@ namespace TasksLecture2
              - a static field `index` of int type
              - create two instances of `Marker` with different colors and index values.
              - Print both instances of marker to Console - Create a method in struct PrintMarker() which will print itself to Console*/
-        
+
             Marker permanent = new Marker("black", 5);
-            Marker whiteBoard = new Marker("blue", 6);
+            Marker whiteBoard = new Marker("blue", 7);
 
-            permanent.PrintMarker("black", 5);
-            whiteBoard.PrintMarker("blue", 2);
+            permanent.PrintMarker();
+            whiteBoard.PrintMarker();
 
-          
-            
-            // 2.Create a static method `InitMarker(string color)` which takes color as an argument and returns created `Marker` instance with that color:
-            static void InitMarker(string color)
-            {
-                Marker2 myMarker = new Marker2(color);
-                Console.WriteLine(myMarker.color);
-            }
-
-            InitMarker("green");
-
+            Console.WriteLine("Enter a colour:");
+            string markerColor = Console.ReadLine();
+            Marker.InitMarker(markerColor);
 
         }
+
 
         //3.Write a program to insert the information of two books.
         public struct Book
@@ -111,68 +126,45 @@ namespace TasksLecture2
         }
 
 
-
         /*1. Create a structure `Marker` which have
          - a `color` filed of string type
          - a static field `index` of int type
          - create two instances of `Marker` with different colors and index values.
          - Print both instances of marker to Console - Create a method in struct PrintMarker() which will print itself to Console*/
         /// <summary>
-        ///  a static field `index` of int type - це не зрозуміла, як зробити.
+        ///  a static field `index` of int type - це поправила!
         /// </summary>
- 
+
         public struct Marker
-         {
-            public string color;
-            public int index;
-            public Marker(string color, int index)
-           {
-
-            this.color = color;
-            this.index = index;
-           }
-            public void PrintMarker(string color, int index)
-            {
-                Console.WriteLine($"This marker has {color} color and index {index}.");
-            }
-        }
-
-        //2.Create a static method `InitMarker(string color)` which takes color as an argument and returns created `Marker` instance with that color:
-        /// <summary>
-        ///  `Nullable<Marker>` - з цим ще не розібралася. 
-        /// </summary>
-        public struct Marker2
         {
             public string color;
-            public Marker2(string color)
+            public static int Index;
+
+            public Marker(string color, int index)
+            {
+                this.color = color;
+                Index = index;
+            }
+
+            public Marker(string color)
             {
                 this.color = color;
             }
+            public void PrintMarker()
+            {
+                Console.WriteLine($"This marker has {color} color and index {Index}.");
+            }
+            public static Marker InitMarker(string color)
+
+            {
+                return new Marker(color);
+            }
+
         }
+
+        //2.Create a static method `InitMarker(string color)` which takes color as an argument and returns created `Marker` instance with that color:
+        //з цією задачею заплуталася...
 
     }
 }
 
-/*
-
-       ЦІ ДВА ЗАВДАННЯ СПРОБУЮ ЩЕ ПІЗНІШЕ ЗРОБИТИ
-
-==== Methods =====
-1.Write a program to create a function to swap the values of two integer numbers.
-Test Data :
-Enter a number: 5
-Enter another number: 6
-Expected Output :
-Now the 1st number is : 6 , and the 2nd number is : 5
-
-2.Create a struct Point (X, Y) and method TryParsePoint, requirements to TryParsePoint:
-    -Valid input is coma - separated value: `2,3` or `8, 88` anything else is considered invalid.
-    -method returns `true` if input is parsed successfully, otherwise - false.
-
-   - method returns parsed `Point` instance via `out` argument. in case of invalid input - `null` is returned.
-Test Data :
-    -Input your point: 8, 88
-   - Input your point: hehe, wrong
-Expected output: 
-    -Result of parsing: true, Point = (8, 88)
-   - Result of parsing: false, Point = undefined*/
