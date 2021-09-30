f439411bd5c75d9f3d66105ad550d591d8b411da
Yuriy Plyska <Yuriyply@gmail.com>
2021-09-30 17:39:39 +0300

    Task4 -done

---
+++ Bootcamp/Lecture4/Homework/IPlyska-Task4/IPlyskaLect4/Phone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPlyskaLect4
+{
+    public class Phone
+    {
+        public int BatteryLevel { get; set; }
+
+        public void Charge()
+        {
+            BatteryLevel = 100;
+            Console.WriteLine($"Charging {this} to 100%");
+        }
+        public void ChargeABit()
+        {
+            BatteryLevel += 1;
+            Console.WriteLine($"Charging {this} by 1%");
+        }
+        public void CallAmbulance()
+        {
+            if (BatteryLevel < 0 || BatteryLevel > 100)
+            {
+                throw new InappropriateBatteryLevelValueException("Charging of battery can not be less than 0 or more 100");
+            }
+
+            if (BatteryLevel >= 5)
+            {
+                BatteryLevel -= 5;
+                Console.WriteLine($"calling an ambulance from {this}, remaining charge: {BatteryLevel}%");
+            }
+            
+            if (BatteryLevel < 5)
+            {
+                BatteryLevel = 0;
+                throw new BatteryIsDeadException(this.ToString());
+            }
+        }
+
+    }
+}
---
+++ Bootcamp/Lecture4/Homework/IPlyska-Task4/IPlyskaLect4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IPlyskaLect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneEmergencyTestHolder phone = new PhoneEmergencyTestHolder();
+            int counter = 1;
+            while (counter != 10)
+            {
+                try
+                {
+                    PhoneEmergencyTestHolder.TestEmergency(phone);       
+                }
+                catch (BatteryIsDeadException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+               counter++;
+            }
+               
+            Console.ReadLine();
+        }
+    }
+}
