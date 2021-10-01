68645a931f1c67e86577c6cd1f860cb7a6f4266f
kateryna.fedak <kateryna.fedak@justanswer.com>
2021-10-01 17:06:31 +0300

    Made task2(Lecture5)

--- Bootcamp/Lecture5/Homework/KFedak-Task5/KFedakTask5/Program.cs
+++ Bootcamp/Lecture5/Homework/KFedak-Task5/KFedakTask5/Program.cs
@@ -7,12 +7,61 @@ namespace KFedakTask5
     {
         public static List<Point> listPoint = new List<Point>();
         public static Circle[] circles;
+        public static List<Person> peopleList = new List<Person>();
+
         static void Main(string[] args)
+        {
+            TaskOne();
+
+            Console.WriteLine("************************************\n");
+
+            TaskTwo();
+        }
+
+        public static void TaskOne()
         {
             CreateObjects();
             Shape.Draw<Point>(listPoint);
             Shape.Draw<Circle>(circles);
         }
+
+        public static void TaskTwo()
+        {
+            Dictionary<string, int> population = new Dictionary<string, int>();
+
+            CreatePerson();
+
+            foreach (var person in peopleList)
+            {
+                if (!population.ContainsKey(person.City))
+                {
+                    population.Add(person.City, 1);
+                }
+                else
+                {
+                    population[person.City] += 1;
+                }
+            }
+
+            foreach (var pair in population)
+            {
+                Console.WriteLine($" *{pair.Key} - {pair.Value}");
+            }
+        }
+
+        public static void CreatePerson()
+        {
+            peopleList.Add(new Person("Oleh", "Lviv"));
+            peopleList.Add(new Person("Katya", "Lviv"));
+            peopleList.Add(new Person("Anna", "Kyiv"));
+            peopleList.Add(new Person("Orest", "Lviv"));
+            peopleList.Add(new Person("Yura", "Kyiv"));
+            peopleList.Add(new Person("Petro", "Odessa"));
+            peopleList.Add(new Person("Oleh", "Kyiv"));
+            peopleList.Add(new Person("Oleh", "Lviv"));
+            peopleList.Add(new Person("Oleh", "Odessa"));
+        }
+
         public static void CreateObjects()
         {
             listPoint.Add(new Point(2.5, 4.3));
