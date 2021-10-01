7b95d5fb7da37786c0667ad3b15a196993793675
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-01 15:52:00 +0300

    added project

---
+++ Bootcamp/Lecture5/Homework/IPlyska-Task5/IPlyskaLect5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IPlyskaLect5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
