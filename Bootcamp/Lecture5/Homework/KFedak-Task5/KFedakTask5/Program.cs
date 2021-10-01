bd14e8ec2abbb53c0ea626edfb796aa7b918bd4f
kateryna.fedak <kateryna.fedak@justanswer.com>
2021-10-01 15:50:13 +0300

    create project

---
+++ Bootcamp/Lecture5/Homework/KFedak-Task5/KFedakTask5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KFedakTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
