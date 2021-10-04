c6e568ff22ce7a1871b9cb084334485f2dd83795
volodymyr herasymenko <volodymyr.herasymenko@justanswer.com>
2021-10-04 19:35:05 +0300

    Herasymenko Lesson6

---
+++ Bootcamp/Lecture5/Homework/Herasymenko_Lesson6/Copart/Auctioneer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Copart
+{
+    class Auctioneer
+    {
+        public event Action<int> OnBidChanged;
+        public static int Bid { get; set; }
+
+        public void SetNewBid(int value)
+        {
+            Bid = value;
+            OnBidChanged?.Invoke(value);
+        }
+
+  //      	1. if bidValue > 100,  print "Ohhh they really want to by this car for {bidValue}?", else do nothing
+	 // 	2. if bidValue< 300 print "I can give more, {bidValue} is nothing.", else do nothing
+		//3. if bidValue< 200 print "I will wait a bit. I think that {bidValue} is not the last price.", else do nothing
+    }
+}
---
+++ Bootcamp/Lecture5/Homework/Herasymenko_Lesson6/Herasymenko_Lesson6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Herasymenko_Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<string>{"A client", "b server", "C server-js", "d client", "E server-dd"};
+
+            var filteredCollection = collection.LetsFilter<string>();
+
+            var mappedCollection = filteredCollection.Map<int>();
+
+            foreach (var item in mappedCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    static class Extension
+    {
+        public static ICollection<T> LetsFilter<T>(this ICollection<string> collection)
+        {
+            var filteredList = new List<string>();
+
+            foreach (var item in collection)
+            {
+                if (char.IsUpper(item[0]))
+                {
+                    filteredList.Add(item);
+                }
+            }
+
+            return (ICollection<T>)filteredList;
+        }
+
+        public static ICollection<T> Map<T>(this ICollection<string> collection)
+        {
+            var mappedList = new List<int>();
+
+            foreach (var item in collection)
+            {
+                mappedList.Add(item.Length);
+            }
+
+            return (ICollection<T>)mappedList; 
+        }
+    }
+}
