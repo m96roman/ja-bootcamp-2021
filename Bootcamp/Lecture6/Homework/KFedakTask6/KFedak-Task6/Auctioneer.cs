f83c701fa2e3962a28f94222fd329c24be40f4d1
KateFedak <katefedakk@gmail.com>
2021-10-04 17:55:52 +0300

    /

---
+++ Bootcamp/Lecture6/Homework/KFedakTask6/KFedak-Task6/Auctioneer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFedak_Task6
+{
+    public class Auctioneer
+    {
+        public int Bid { get; set; }
+
+        public event Action<int> OnBidChanged;
+        public Auctioneer(int bid)
+        {
+            this.Bid = bid;
+        }
+        public void SetNewBid(int number)
+        {
+            var oldBid = Bid;
+            Bid = number;
+            OnBidChanged?.Invoke(number);
+        }
+    }
+}
---
+++ Bootcamp/Lecture6/Homework/KFedakTask6/KFedak-Task6/ExtensionMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFedak_Task6
+{
+    public static class ExtensionMethod
+    {
+        public static ICollection<T> Filter<T>(this ICollection<T> collection, Func<T, bool> filterText) => collection.Where(filterText).ToList();
+
+        public static ICollection<V> Map<T, V>(this ICollection<T> collection, Func<T, V> filterText) => collection.Select(source => filterText(source)).ToArray();
+    }
+}
