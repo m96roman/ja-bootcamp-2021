ada06d9e7670ca6289d5bbf88c860eb08642cfbb
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-10-04 14:01:00 +0300

    init

---
+++ Bootcamp/Lecture6/Homework/ivanyshyn/DIvanyshyn-6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIvanyshyn_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
