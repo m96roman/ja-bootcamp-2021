63057550d14d32c43b3e2363a9cf5e33b3446d2d
Nestor Pylat <nestor.pylat@justanswer.com>
2021-10-04 17:48:55 +0300

    Task 2

---
+++ Bootcamp/Lecture6/Homework/Task 2/Task 2/Auctioneer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public class Auctioneer
+    {
+
+        public int Bid { get; private set; }
+        public event Action<int> OnBidChanged;
+
+        public void SetNewBid(int num)
+        {
+            Bid = num;
+            OnBidChanged?.Invoke(Bid);
+            Bid = 4;
+            OnBidChanged?.Invoke(Bid);
+            Bid = 400;
+            OnBidChanged?.Invoke(Bid);
+            Bid = 200;
+            OnBidChanged?.Invoke(Bid);
+        }
+    }
+
+    class Auctioneer1
+    {
+
+
+    }
+}
--- Bootcamp/Lecture6/Homework/Task 2/Task 2/Program.cs
+++ Bootcamp/Lecture6/Homework/Task 2/Task 2/Program.cs
@@ -6,7 +6,35 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Auctioneer auctioneer = new Auctioneer();
+            auctioneer.OnBidChanged += Auctioneer_OnBidChanged;
+            auctioneer.SetNewBid(123);
+            
+        }
+
+        private static void Auctioneer_OnBidChanged(int obj)
+        {
+            // 1 
+
+            if (obj > 100)
+            {
+                Console.WriteLine($"Ohhh they really want to by this car for { obj}?");
+            }
+
+            // 2
+
+            if (obj < 300)
+            {
+                Console.WriteLine($"I can give more, {obj} is nothing.");
+            }
+
+            // 3
+
+            if(obj<200)
+            {
+                Console.WriteLine($"I will wait a bit.I think that { obj } is not the last price.");
+            }
+            Console.WriteLine(obj);
         }
     }
 }
