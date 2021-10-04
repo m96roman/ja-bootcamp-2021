35cce3c32fd2c6382489c6de49b2499b56978008
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-04 18:15:27 +0300

    task6

---
+++ Bootcamp/Lecture6/Homework/IPlyska-Task6/IPlyskaLect6/ExtentionsCollecton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPlyskaLect6
+{
+    
+    public static class ExtentionsCollecton
+    {
+        public static ICollection<string> Filter(this ICollection<string> container, Func<string, bool> func )
+        {
+            if (container is null || container.Count == 0)
+            {
+                return null;
+            }
+
+            var result = new List<string>();
+
+            foreach (var item in container)
+            {
+                if ((func?.Invoke(item)).HasValue && (func?.Invoke(item)).Value)
+                {
+                    result.Add(item);
+                }  
+            }
+
+            return result;
+            //return container.Select(x => x.Trim()).Where(x => char.IsUpper(x.First())).ToList();
+        }
+
+        public static ICollection<int> Map(this ICollection<string> mapper, Func<string, int> func)
+        {
+            if (mapper is null || mapper.Count == 0)
+            {
+                return null;
+            }
+
+            var result = new List<int>();
+
+            foreach (var item in mapper)
+            {
+                result.Add((func?.Invoke(item)).Value);
+            }
+
+            return result;
+            //return mapper.Where(x => !string.IsNullOrEmpty(x)).Select(x => Convert.ToInt32(x.Length)).ToList();
+        }
+
+        public static ICollection<T> Filter<T>(this ICollection<T> container, Func<T,T> action) where T : struct
+        {
+            if (container is null || container.Count == 0)
+            {
+                return null;
+            }
+           
+           return container.Select(x => (action?.Invoke(x)).Value).ToList();
+        }
+
+        public static ICollection<T> Map<T>(this ICollection<T> mapper, Func<T, T> action) 
+        {
+            if (mapper is null || mapper.Count == 0)
+            {
+                return null;
+            }
+
+            return mapper.Select(x => (action.Invoke(x))).ToList();
+        }
+    }
+}
---
+++ Bootcamp/Lecture6/Homework/IPlyska-Task6/IPlyskaLect6/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IPlyskaLect6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new List<string>()
+            {
+                "Milk",
+                "",
+                "cow",
+                "Ball"
+            };
+            //var result = container.Filter(StringToUpper);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var result2 = container.Map(StringLenght);
+
+            //foreach (var item in result2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //List<char> charContainer = new List<char>()
+            //{
+            //    '3',
+            //    'c',
+            //    '5',
+            //    '6',
+            //    'U'
+            //};
+
+            //var result3 = charContainer.Filter<char>(IsCharDigit);
+
+            //foreach (var item in result3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var result4 = container.Map<string>(StringLowerCase);
+
+            //foreach (var item in result4)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            /////////////TASK2////////////////
+            HandlerWithFirst handler1 = new HandlerWithFirst();
+            HandlerWithSecond handler2 = new HandlerWithSecond();
+            HandlerWithThird handler3 = new HandlerWithThird();
+
+            Auctioneer auctioneer = new Auctioneer();
+
+            auctioneer.OnBidChanged += handler1.HandleSetNewBidWith;
+            auctioneer.OnBidChanged += handler2.HandleSetNewBidWith;
+            auctioneer.OnBidChanged += handler3.HandleSetNewBidWith;
+
+            auctioneer.SetNewBid(150);
+
+        }
+
+        public static bool StringToUpper(string input)
+        {
+            if (string.IsNullOrEmpty(input) || !char.IsUpper(input[0]))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int StringLenght(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            return input.Length;
+        }
+
+        public static char IsCharDigit(char input)
+        {
+            if (char.IsDigit(input))
+            {
+                return input;
+            }
+            return ' ';
+        }
+
+        public static string StringLowerCase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            return input.ToLower();
+
+        }
+      
+    }
+}
