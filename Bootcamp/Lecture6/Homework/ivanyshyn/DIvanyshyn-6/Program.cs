b7721c74068e480b99b0f177afff8fb10af5bde7
Dmytro <dmutroivd@gmail.com>
2021-10-05 08:04:16 +0300

    Rebuilded

--- Bootcamp/Lecture6/Homework/ivanyshyn/DIvanyshyn-6/Program.cs
+++ Bootcamp/Lecture6/Homework/ivanyshyn/DIvanyshyn-6/Program.cs
@@ -8,9 +8,9 @@ namespace DIvanyshyn_6
     {
         static void Main(string[] args)
         {
-            //Task1.Execute();
+            Task1.Execute();
 
-            Task2.Execute();
+            //Task2.Execute();
         }
     }
 }
