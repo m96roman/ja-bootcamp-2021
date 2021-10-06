231db2c6734621f753841c94c07249a725396bc9
Martin Roshko <martin.roshko1@gmail.com>
2021-10-06 14:36:10 +0300

    Homework-Task8

---
+++ Bootcamp/Lecture8/Homework/MRoshko-Task8/Api.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace MRoshko_Task8
+{
+    class Api
+    {
+        public Controller controller { get; set; }
+
+        public Api()
+        {
+            controller = new Controller();
+        }
+
+
+        public void CallEndpoint(string route)
+        {
+            controller.Name = route;
+
+            var colectionsAtribue = controller
+                .GetType()
+                .GetMethods()
+                .Select(s => new { methods = s, atribute = s.GetCustomAttribute<RouteAttribute>() })
+                .Where(p => p.atribute != null);
+
+            var currentMethod = colectionsAtribue
+                .FirstOrDefault(atr => atr.atribute.Name == route);
+
+            Console.WriteLine(currentMethod.methods.Invoke(controller, null));
+        }
+    }
+}
