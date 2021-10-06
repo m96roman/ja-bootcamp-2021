4d95ce354be23b44d9412b7e3001c0fc5aead85f
Yevhen Shyptur <eugeni.shyptur@gmail.com>
2021-10-06 17:50:46 +0300

    Shyptur - Task8

---
+++ Bootcamp/Lecture8/Homework/Shyptur - Task8/Shyptur-Task8HW/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace Shyptur_Task8HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1 task1 = new Task1();
+            //task1.task1();
+
+            var memoryStreamWrapper = new DisposableObject();
+            memoryStreamWrapper.DoSomething();
+            memoryStreamWrapper.Dispose();
+            try
+            {
+                memoryStreamWrapper.DoSomething();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
+        }
+    }
+    class DisposableObject : IDisposable
+    {
+        private bool _object;
+
+        public DisposableObject()
+        {
+            _object = true;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("I was disposed = (");
+            _object = false;
+        }
+
+        public void DoSomething()
+        {
+            if (_object)
+            {
+                Console.WriteLine("I'm doing something important.");
+            }
+            else
+            {
+                throw new ObjectDisposedException("I was disposed ");
+            }
+        }
+    }
+}
---
+++ Bootcamp/Lecture8/Homework/Shyptur - Task8/Shyptur-Task8HW/Task1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Shyptur_Task8HW
+{
+    class Task1
+    {
+        public void task1()
+        {
+            var api = new Api();
+            api.CallEndpoint("/controller/get");
+            api.CallEndpoint("/controller/post");
+            api.CallEndpoint("/controller/post");
+            api.CallEndpoint("/controller/get");
+            api.CallEndpoint("/controller/put");
+            api.CallEndpoint("/controller/delete");
+        }
+    }
+}
+[AttributeUsage(AttributeTargets.Method)]
+public class RouteAttribute : Attribute
+{
+    public string Name { get; set; }
+}
+public class Controller
+{
+    public string Name { get; set; }
+
+    [Route(Name = "/controller/get")]
+    public int Get()
+    {
+        return 8;
+    }
+
+    [Route(Name = "/controller/post")]
+    public void Post()
+    {
+        Console.WriteLine($"Post method executed in {Name} controller");
+    }
+
+    [Route(Name = "/controller/put")]
+    public void Put()
+    {
+        Console.WriteLine($"Put method executed in {Name} controller");
+    }
+
+    [Route(Name = "/controller/delete")]
+    public void Delete()
+    {
+        Console.WriteLine($"Delete method execute in {Name} controller");
+    }
+}
+
+public class Api
+{
+    public Controller controller { get; set; }
+
+    public Api()
+    {
+        controller = new Controller();
+    }
+
+    public void CallEndpoint(string route)
+    {
+        controller.Name = route;
+        var type = controller.GetType();
+        var colectionsAtribue = type
+            .GetMethods()
+            .Select(s => new
+            {
+                method = s,
+                atribute = s.GetCustomAttribute<RouteAttribute>()
+            })
+            .Where(p => p.atribute != null)
+            .ToList();
+
+        var currentMethod = colectionsAtribue.FirstOrDefault(atr => atr.atribute.Name == route);
+
+        Console.WriteLine(currentMethod.method.Invoke(controller, null));
+    }
+}
