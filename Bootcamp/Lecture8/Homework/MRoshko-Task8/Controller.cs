1a3365d0adc2e8e9557bc32e65af05810a5cb5ce
Martin Roshko <martin.roshko1@gmail.com>
2021-10-06 14:36:10 +0300

    Homework-Task8

---
+++ Bootcamp/Lecture8/Homework/MRoshko-Task8/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRoshko_Task8
+{
+    public class Controller
+    {
+        public string Name { get; set; }
+
+        [Route("/controller/get")]
+        public int Get() 
+        {
+            return 8;    
+        }
+
+        [Route("/controller/post")]
+        public void Post()
+        {
+            Console.WriteLine($"Post method executed in {Name} controller");
+        }
+
+        [Route("/controller/put")]
+        public void Put() 
+        {
+            Console.WriteLine($"Put method executed in {Name} controller");
+        }
+
+        [Route("/controller/delete")]
+        public void Delete()
+        {
+            Console.WriteLine($"Delete method execute in {Name} controller");
+        }
+
+    }
+}
---
+++ Bootcamp/Lecture8/Homework/MRoshko-Task8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MRoshko_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1();
+
+            Task2();
+        }
+
+        public static void Task1()
+        {
+            var api = new Api();
+
+            api.CallEndpoint("/controller/get");
+            api.CallEndpoint("/controller/post");
+            api.CallEndpoint("/controller/post");
+            api.CallEndpoint("/controller/get");
+            api.CallEndpoint("/controller/put");
+            api.CallEndpoint("/controller/delete");
+        }
+
+        public static void Task2()
+        {
+            var disposableObject = new DisposableObject();
+
+            disposableObject.Dispose();
+
+            try
+            {
+                disposableObject.DoSomething();
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+    }
+}
+
