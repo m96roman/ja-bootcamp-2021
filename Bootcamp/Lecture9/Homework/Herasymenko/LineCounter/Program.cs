7e036817a0647c521ac14b462d63f351ce393134
volodymyr herasymenko <volodymyr.herasymenko@justanswer.com>
2021-10-07 23:41:08 +0300

    Herasymenko Lesson9 without 4th task

---
+++ Bootcamp/Lecture9/Homework/Herasymenko/LineCounter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LineCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentPath = Directory.GetCurrentDirectory();
+            var currentFile = @$"{currentPath}\file.txt";
+            var linesCount = CountLiner(currentFile);
+
+            Console.WriteLine(linesCount);
+
+        }
+
+        public static long CountLiner(string file)
+        {
+            if(!File.Exists(file))
+            {
+                throw new Exception("The file doesn't exists");
+            }
+
+            var lineCounter = 0L;
+
+            using (var reader = new StreamReader(file))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    lineCounter++;
+                }
+                return lineCounter;
+            }
+        }
+    }
+}
