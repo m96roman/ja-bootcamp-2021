f35c0c7c41cbe78e67a52a5fe47f179459346ec4
oleh.krutii <oleh.krutii@justanswer.com>
2021-10-07 10:51:53 +0300

    add homework and LiveCoding project

---
+++ Bootcamp/Lecture9/LiveCoding9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.Json;
+
+namespace LiveCoding9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
