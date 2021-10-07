1f2500368b55640be4df36a7c84da010623655f6
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-10-07 13:53:49 +0300

    Init(Task1 done)

---
+++ Bootcamp/Lecture9/Homework/ivanyshyn/DIvanyshyn-9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DIvanyshyn_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCounter.Task1.Execute();
+
+            //Task2.Task2.Execute();
+
+            //Task3.Task3.Execute();
+
+            //Task4.Task4.Execute();
+        }
+    }
+}
---
+++ Bootcamp/Lecture9/Homework/ivanyshyn/DIvanyshyn-9/Task4/Task4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIvanyshyn_9.Task4
+{
+    class Task4
+    {
+        internal static void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
