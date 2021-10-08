03563bf0222eaa7c0507b8c56c53b667e8ebd201
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-08 10:00:20 +0300

    added task4

--- Bootcamp/Lecture9/Homework/IPlyska-Task9/IPlyskaLect9/Program.cs
+++ Bootcamp/Lecture9/Homework/IPlyska-Task9/IPlyskaLect9/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IPlyskaLect4;
+using System;
 using System.Collections.Generic;
 
 namespace IPlyskaLect9
@@ -9,22 +10,50 @@ namespace IPlyskaLect9
         {
             //FileWorker.ReadFromConsole();
 
-            List<Student> students = Student.DeSerialize();
-            if (students.Count == 0)
+            //List<Student> students = Student.DeSerialize();
+            //if (students.Count == 0)
+            //{
+            //    students = new List<Student>
+            //    {
+            //       new Student { LecturesAttended = 1, Name = "Ivan"},
+            //       new Student { LecturesAttended = 3, Name = "Max"},
+            //       new Student { LecturesAttended = 4, Name = "Loki"},
+            //       new Student { LecturesAttended = 6, Name = "Thor"},
+            //       new Student { LecturesAttended = 7, Name = "Vika"},
+            //       new Student { LecturesAttended = 18, Name = "Ivan"},
+            //    };
+            //}
+
+            //Student.SemesterEmulation(students);
+            //Student.SerializeStudents(students);
+
+            List<Phone> container = new List<Phone>()
             {
-                students = new List<Student>
+                new Nokia{BatteryLevel = 5},
+                new IPhone13{BatteryLevel = 10},
+                new Nokia{BatteryLevel = 25},
+                new IPhone13{BatteryLevel = 8},
+                new Nokia{BatteryLevel = 12}
+            };
+
+            PhoneEmergencyTestHolder phone = new PhoneEmergencyTestHolder(container);
+            int counter = 1;
+            while (counter != 10)
+            {
+                try
                 {
-                   new Student { LecturesAttended = 1, Name = "Ivan"},
-                   new Student { LecturesAttended = 3, Name = "Max"},
-                   new Student { LecturesAttended = 4, Name = "Loki"},
-                   new Student { LecturesAttended = 6, Name = "Thor"},
-                   new Student { LecturesAttended = 7, Name = "Vika"},
-                   new Student { LecturesAttended = 18, Name = "Ivan"},
-                };
+                    PhoneEmergencyTestHolder.TestEmergency(phone);
+                }
+                catch (BatteryIsDeadException ex)
+                {
+                    Logger.WriteLine(ex.ToString());
+                    ex.phone.Charge();
+                }
+
+                counter++;
             }
 
-            Student.SemesterEmulation(students);
-            Student.SerializeStudents(students);
+            Console.ReadLine();
 
         }
     }
