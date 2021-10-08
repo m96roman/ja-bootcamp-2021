97d83439f0dbd7ab3ed04e060fc22aac6a526c07
Yevhen Shyptur <eugeni.shyptur@gmail.com>
2021-10-08 16:36:18 +0300

    task1

---
+++ Bootcamp/LectureF10/Homework/Shyptur - Task10/Shyptur Task10HW/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace Shyptur_Task10HW
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //  Task1();
+
+            var httpClinet = new HttpClient();
+            var text = await httpClinet.GetStringAsync("https://www.gutenberg.org/files/30155/30155-0.txt");
+            Console.WriteLine(text);
+        }
+
+        static void Task1()
+        {
+            Git git = new Git();
+            var listOfThreads = new List<Task>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                listOfThreads.Add(Task.Run(() => PushUse(git)));
+            }
+
+            Task.WaitAll(listOfThreads.ToArray());
+            Console.WriteLine(git.commits.Count);
+        }
+
+        public static void PushUse(Git git)
+        {
+            for (int i = 0; i < 88; i++)
+            {
+                git.Push(i.ToString());
+            }
+        }
+    }
+}
