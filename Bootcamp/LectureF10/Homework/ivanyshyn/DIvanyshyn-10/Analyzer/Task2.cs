3eba32d94992a9c9c8a03b6f7c1ba39ffdc1fe59
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-10-08 13:45:01 +0300

    init

---
+++ Bootcamp/LectureF10/Homework/ivanyshyn/DIvanyshyn-10/Analyzer/Task2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIvanyshyn_10.Analyzer
+{
+    class Task2
+    {
+        public static void Execute()
+        {
+
+        }
+    }
+}
---
+++ Bootcamp/LectureF10/Homework/ivanyshyn/DIvanyshyn-10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIvanyshyn_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GitSimulator.Task1.Execute();
+
+            //Analyzer.Task2.Execute();
+        }
+    }
+}
