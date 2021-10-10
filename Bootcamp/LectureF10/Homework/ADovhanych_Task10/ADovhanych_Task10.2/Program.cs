9f1dccf84eb91fb574b401003f61566e78a0d5a3
tennesseee <antdvgi@gmail.com>
2021-10-10 22:07:05 +0300

    Update Task 10

--- Bootcamp/LectureF10/Homework/ADovhanych_Task10/ADovhanych_Task10.2/Program.cs
+++ Bootcamp/LectureF10/Homework/ADovhanych_Task10/ADovhanych_Task10.2/Program.cs
@@ -10,16 +10,54 @@ namespace ADovhanych_Task10._2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            await Handler();
+        }
+
+        static async Task Handler()
         {
             var httpClient = new HttpClient();
-            var regExp = new Regex(@"[^\p{L}]*\p{Z}[^\p{L}]*");
-            Task<string> file = httpClient.GetStringAsync("https://www.gutenberg.org/files/30155/30155-0.txt"); 
-            List<string> words = new List<string>(file.Result.Split()); 
+            string file = await httpClient.GetStringAsync("https://www.gutenberg.org/files/30155/30155-0.txt");
+            List<string> words = new List<string>(file
+                .Split(new char[] { '\t', '\n', '\r', '\\', '/', ' ', ',', '.', ':', '-' }, StringSplitOptions.RemoveEmptyEntries));
 
-            string longest = words.OrderByDescending(s => s.Length).First();
+            Parallel.Invoke(() =>
+            {
+                string dir = Directory.GetCurrentDirectory();
+                string destination = Path.Combine(dir, "Book.txt");
 
-            Console.WriteLine(longest);
+                File.WriteAllTextAsync(destination, file);
+                Console.WriteLine($"Text is saved to Book.txt in {destination}");
+
+            }, () =>
+            {
+                string longest = words.OrderByDescending(s => s.Length).First();
+                Console.WriteLine($"The longest word in the file is: {longest}");
+            }, () =>
+            {
+                int counter = words.Count(word => word.Equals("Relativity"));
+                Console.WriteLine($"Relativity is used: {counter} times");
+            }, () =>
+            {
+                var topCommonWords = words
+                   .GroupBy(word => word)
+                   .Select(word => new
+                   {
+                       Field = word.Key,
+                       Count = word.Count()
+                   })
+                   .OrderByDescending(word => word.Count)
+                   .Take(8);
+
+                int place = 1;
+
+                foreach(var item in topCommonWords)
+                { 
+                    Console.WriteLine($"Top {place} most used word is {item.Field} - {item.Count}");
+                    place++;
+                }
+            });
         }
-    }
+    } 
 }
