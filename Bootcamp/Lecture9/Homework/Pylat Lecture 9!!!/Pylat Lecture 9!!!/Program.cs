7cbaf75885f9e75a8860318c0f122b8fcd3cb37a
Nestor Pylat <nestor.pylat@justanswer.com>
2021-10-11 10:13:35 +0300

    Pylat Lecture 9

---
+++ Bootcamp/Lecture9/Homework/Pylat Lecture 9!!!/Pylat Lecture 9!!!/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Pylat_Lecture_9_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Task 1 //////////////
+
+            Task1.CreateFileTxt();
+            Console.WriteLine(Task1.CountLineInFile("PylatLecture9"));
+
+            /*// Task 2 //////////////////////
+
+            Task2.doTask2();*/
+
+            /*//   Task 3 ///////////////////////
+
+            DoTask3.SimulateSemestr();*/
+
+        }
+    }
+}
---
+++ Bootcamp/Lecture9/Homework/Pylat Lecture 9!!!/Pylat Lecture 9!!!/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Pylat_Lecture_9_Task_1
+{
+    class Task1
+    {
+        public static string CreateFileTxt()
+        {
+            var directory = Directory.GetCurrentDirectory();
+            Console.WriteLine(directory);
+            using (var newFile = File.Create($"{directory}\\PylatLecture9")) {
+                using (var writer = new StreamWriter(newFile))
+                {
+                    writer.WriteLine(@"Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text
+                                   Text text text text ");
+
+                    return newFile.Name;
+                } 
+            }  
+        }
+
+        public static int CountLineInFile(string file)
+        {
+            var lineCounter = 0;
+            using (var reader = new StreamReader(file))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    lineCounter++;
+                }
+            }
+            return lineCounter;
+        }
+    }
+}
+
