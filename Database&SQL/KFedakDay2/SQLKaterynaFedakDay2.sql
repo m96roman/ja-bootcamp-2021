84607c45428a3358b5956249304ec5c84a369010
KateFedak <katefedakk@gmail.com>
2021-10-12 18:10:50 +0300

    SQLKFedakDay2

--- Database&SQL/KFedakDay2/SQLKaterynaFedakDay2.sql
+++ Database&SQL/KFedakDay2/SQLKaterynaFedakDay2.sql
@@ -1,29 +1,49 @@
-SELECT SUM(Distance) AS 'Total distance'
+USE BootcampTraining;
+
+SELECT 
+	SUM(Distance) 'Total distance'
 FROM Trip
 
-SELECT c.Name,MAX(MaxSpeed) as 'Max speed' 
+SELECT 
+	c.Name,
+	MAX(MaxSpeed) 'Max speed' 
 FROM CarModel m
-JOIN CarBrand as c on m.BrandId=c.ID
+JOIN CarBrand c 
+	ON m.BrandId=c.ID
 GROUP BY c.Name;
 
 
-SELECT c.Name,MAX(MaxSpeed) as 'Max speed' 
+SELECT 
+	c.Name,
+	MAX(MaxSpeed) 'Max speed' 
 FROM CarModel m
-JOIN CarBrand AS c ON m.BrandId=c.ID
+JOIN CarBrand c 
+	ON m.BrandId=c.ID
 WHERE m.BrandId IN (SELECT BrandId FROM CarModel GROUP BY BrandId HAVING COUNT(*)>1)
 GROUP BY c.Name
 ORDER BY 'Max speed' DESC
 
-SELECT ID,Name FROM CarModel
-UNION 
-SELECT ID,Name FROM CarBrand
-ORDER BY ID ;
-
-SELECT b.Name,m.Name, DateDiff(MINUTE, t.StartDate, t.EndDate) , t.Distance
+SELECT 
+	b.ID,
+	b.Name 
+FROM CarBrand b
+UNION
+SELECT 
+	m.ID,
+	m.Name 
 FROM CarModel m
-JOIN CarBrand AS b on m.BrandId=b.ID
-JOIN Car AS c ON c.ModelId=m.ID
-JOIN Trip AS t ON t.CarId=c.ID
+ORDER BY b.ID
+
+SELECT 
+	b.Name,
+	m.Name
+FROM CarBrand b
+JOIN CarModel m
+	ON m.BrandId=b.ID
+JOIN Car c 
+	ON c.ModelId=m.ID
+JOIN Trip t 
+	ON t.CarId=c.ID
 WHERE (t.StartDate IS NULL AND t.EndDate IS NULL) OR  DateDiff(HOUR, t.StartDate, t.EndDate)<1
 
 
