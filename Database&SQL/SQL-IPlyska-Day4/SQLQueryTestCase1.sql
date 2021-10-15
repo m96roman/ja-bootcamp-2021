04400c64e68bfcbff8233e5ebca17e17a02b0163
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-15 17:56:15 +0300

    added test cases

---
+++ Database&SQL/SQL-IPlyska-Day4/SQLQueryTestCase1.sql
@@ -0,0 +1,33 @@
+﻿use BootcampTraining;
+
+exec tSQLt.DropClass 'Test_spUnitTestClassIPlyska'​
+go
+
+exec tSQLt.NewTestClass 'Test_spUnitTestClassIPlyska'
+go
+
+create procedure Test_spUnitTestClassIPlyska.[test 1, happy path]
+
+as begin
+
+	create table #tempTable(mealName nvarchar(40), ingredientName nvarchar(40), timeToCoo int, timeToCookChar nvarchar(40))
+
+	create table #expected(mealName nvarchar(40), ingredientName nvarchar(40), timeToCoo int, timeToCookChar nvarchar(40))
+	select top 0 * into #actual FROM #expected;​
+
+	insert into #expected (mealName, ingredientName, timeToCoo, timeToCookChar)
+	values(N'OMELET EGG ROLL', N'Egg', 15, N'15 as string');
+
+	insert into #tempTable exec sp_ConvertToChar_IPlyska
+
+	insert into #actual
+	select * from #tempTable
+	where mealName = 'OMELET EGG ROLL' and ingredientName = 'Egg'
+
+	 exec tSQLt.AssertEqualsTable #expected, #actual​
+end
+go
+
+exec tSQLt.Run 'Test_spUnitTestClassIPlyska.[test 1, happy path]'
+go
+ 
---
+++ Database&SQL/SQL-IPlyska-Day4/SQLQueryTestCase2.sql
@@ -0,0 +1,41 @@
+﻿use BootcampTraining;
+
+exec tSQLt.DropClass 'Test_spUnitTestClassIPlyska'​
+go
+
+exec tSQLt.NewTestClass 'Test_spUnitTestClassIPlyska'
+go
+
+create procedure Test_spUnitTestClassIPlyska.[Setup]​
+
+as begin​
+
+   exec tSQLt.FakeTable 'MealFake', @Identity=1​
+
+   exec tSQLt.FakeTable 'RecipeFake'​
+
+   
+
+end​
+go
+
+
+create procedure Test_spUnitTestClassIPlyska.[test 2, happy path]
+
+as begin​
+		
+	  create table #Expected (Id int, name nvarchar(40), timeToCook int )​
+	  --insert into #Expected (Id, name, timeToCook)​
+   --   values(1, N'CRISPY CHEESE BITES', 20)​
+
+	  create table #Expected2 (MealId int, IngrId int, Weight int )​
+	  --insert into #Expected (Id, name, timeToCook)​
+   --   values(1, N'CRISPY CHEESE BITES', 20)​
+
+       insert into #Expected exec sp_Add_IPlyska(1, N'CRISPY CHEESE BITES', 20)
+
+end
+
+select * from Meal
+
+select * from Recipe
