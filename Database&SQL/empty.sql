78eae235108d2736e41b2cc69a9f006537520229
Martin Roshko <martin.roshko1@gmail.com>
2021-10-18 10:17:18 +0300

    Final Version

--- Database&SQL/empty.sql
+++ Database&SQL/empty.sql
@@ -1,53 +1,37 @@
 USE BootcampTraining
 
-EXEC tSQLt.DropClass 'Test_spMartin_Procedure'
+EXEC tSQLt.DropClass 'Test_spMartin_Procedure';
 GO
 
-EXEC tSQLt.NewTestClass 'Test_spMartin_Procedure'
+EXEC tSQLt.NewTestClass 'Test_spMartin_Procedure';
 GO
 
 CREATE OR ALTER PROCEDURE Test_spMartin_Procedure.[Setup]
 AS
 BEGIN
-	EXEC tSQLt.FakeTable'CarModel' , @Identity =1
+	EXEC tSQLt.FakeTable'CarModel' , @Identity =1;
 END
 GO
 
 CREATE OR ALTER PROCEDURE Test_spMartin_Procedure.[test 1. raise error if not exist @BrandID]
 AS
 BEGIN 
-	
-	EXEC  sp_insert_martin 1,Q4,400
+	EXEC  sp_insert_martin 1,Q4,400;
 END
 GO
 
-CREATE OR ALTER PROCEDURE Test_spMartin_Procedure.[test 2. raise error if not exist @BrandID]
+CREATE OR ALTER PROCEDURE Test_spMartin_Procedure.[test 2. correct select error]
 AS
 BEGIN 
-    SELECT Trip.Distance INTO #Expected FROM Trip;
-	INSERT INTO #Expected 
-	VALUES (
-		'1285 as string',
-		'952 as string',
-		'1416 as string',
-		'772 as string',
-		'1179 as string')
-
-	CREATE TABLE #A (Distance VARCHAR(40))
-	INSERT INTO #A  EXEC sp_convert_to_string_martin;
-
-
 	CREATE TABLE #Actual (Distance VARCHAR(40))
-	INSERT INTO #Actual 
-	SELECT TOP 5 #A.Distance 
-	FROM #A 
-
-
-	SELECT * FROM #Actual
+	INSERT INTO #Actual  EXEC sp_convert_to_string_martin;
 	
+	CREATE TABLE #A (Distance VARCHAR(40))
+	INSERT INTO #A 
+	SELECT CONCAT(CONVERT(VARCHAR(40),Trip.Distance), ' as string')  
+	FROM Trip ;
 
-	--CREATE TABLE #Actual (SELECT * EXEC sp_convert_to_string_martin)
-	EXEC tSQLt.AssertEqualsTable #Expected , #Actual;
+	EXEC tSQLt.AssertEqualsTable #Actual , #A;
 END
 GO
 
@@ -58,17 +42,17 @@ BEGIN
 
 	SELECT TOP 0 * INTO #Expected FROM CarModel;
 	INSERT INTO #Expected
-	VALUES (1,'Q4',400),(2,'Supra',120)
+	VALUES (1,'Q4',400),(2,'Supra',120);
 
-	EXEC sp_insert_martin 1,Q4,400
-	EXEC sp_insert_martin 2,'Supra',120
+	EXEC sp_insert_martin 1,Q4,400;
+	EXEC sp_insert_martin 2,'Supra',120;
 
-	EXEC tSQLt.AssertEqualsTable #Expected , CarModel
+	EXEC tSQLt.AssertEqualsTable #Expected , CarModel;
+
 END
 GO
 
-EXECUTE tSQLt.Run 'Test_spMartin_Procedure'
+EXECUTE tSQLt.Run 'Test_spMartin_Procedure';
 GO
 
-EXEC sp_convert_to_string_martin
 
