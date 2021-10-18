df4ad54ac4a5821925f7c2cd8b17961cb90b8361
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-18 16:27:26 +0300

    add 4 project

---
+++ Testing/UnitTesting/IPlyska-Task4/IPlyskaLect4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IPlyskaLect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Phone> container = new List<Phone>()
+            {
+                new Nokia{BatteryLevel = 5},
+                new IPhone13{BatteryLevel = 10},
+                new Nokia{BatteryLevel = 25},
+                new IPhone13{BatteryLevel = 8},
+                new Nokia{BatteryLevel = 12}
+            };
+
+            PhoneEmergencyTestHolder phone = new PhoneEmergencyTestHolder(container);
+            int counter = 1;
+            while (counter != 10)
+            {
+                try
+                {
+                    PhoneEmergencyTestHolder.TestEmergency(phone);       
+                }
+                catch (BatteryIsDeadException ex)
+                {
+                    ex.phone.Charge(); 
+                }
+
+               counter++;
+            }
+               
+            Console.ReadLine();
+        }
+    }
+}
