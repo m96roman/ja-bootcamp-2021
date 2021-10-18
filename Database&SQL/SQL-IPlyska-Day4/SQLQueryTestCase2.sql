84d0ce43a54fa3077df9900a9784ae1d85e4ee71
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-18 11:23:39 +0300

    updates

--- Database&SQL/SQL-IPlyska-Day4/SQLQueryTestCase2.sql
+++ Database&SQL/SQL-IPlyska-Day4/SQLQueryTestCase2.sql
@@ -10,32 +10,71 @@ create procedure Test_spUnitTestClassIPlyska.[Setup]​
 
 as begin​
 
-   exec tSQLt.FakeTable 'MealFake', @Identity=1​
+   exec tSQLt.FakeTable 'Meal', @Identity=1​
 
-   exec tSQLt.FakeTable 'RecipeFake'​
-
-   
+   exec tSQLt.FakeTable 'Recipe'​
 
 end​
 go
 
 
-create procedure Test_spUnitTestClassIPlyska.[test 2, happy path]
+alter procedure Test_spUnitTestClassIPlyska.[test 2, happy path]
 
 as begin​
 		
-	  create table #Expected (Id int, name nvarchar(40), timeToCook int )​
-	  --insert into #Expected (Id, name, timeToCook)​
-   --   values(1, N'CRISPY CHEESE BITES', 20)​
+	  create table #Expected1 (Id int, name nvarchar(40), TimeToCookInMins int )​
+	  insert into #Expected1 (Id, name, TimeToCookInMins)​
+      values(1, N'CRISPY CHEESE BITES', 20)​;
 
-	  create table #Expected2 (MealId int, IngrId int, Weight int )​
-	  --insert into #Expected (Id, name, timeToCook)​
-   --   values(1, N'CRISPY CHEESE BITES', 20)​
+	  create table #Expected2 (MealId int, IngredientId int, Weight int )​
+	  insert into #Expected2 (MealId, IngredientId, Weight)​
+      values(3,4,300)​;
 
-       insert into #Expected exec sp_Add_IPlyska(1, N'CRISPY CHEESE BITES', 20)
+      exec sp_Add_IPlyska 'CRISPY CHEESE BITES', 20, 3, 4,300
+
+	  exec tSQLt.AssertEqualsTable #Expected1, Meal
+
+	  exec tSQLt.AssertEqualsTable #Expected2, Recipe
+
 
 end
+go
+
+alter procedure Test_spUnitTestClassIPlyska.[test 1, happy path]
+
+as begin
+
+	create table #tempTable(mealName varchar(40), ingredientName varchar(40), timeToCoo int, timeToCookChar varchar(40))
+
+	create table #expected(mealName varchar(40), ingredientName varchar(40), timeToCoo int, timeToCookChar varchar(40))
+	select top 0 * into #actual FROM #expected;​
+
+	insert into #expected (mealName, ingredientName, timeToCoo, timeToCookChar)
+	values('OMELET EGG ROLL', N'Egg', 15, '15 as string');
+
+	insert into #tempTable exec sp_ConvertToChar_IPlyska
+
+	insert into #actual
+	select * from #tempTable with(nolock)
+	where mealName = 'OMELET EGG ROLL' and ingredientName = 'Egg'
+
+	exec tSQLt.AssertEqualsTable #expected, #actual​
+end
+go
+
+
+exec tSQLt.Run'Test_spUnitTestClassIPlyska'
 
 select * from Meal
 
 select * from Recipe
+
+sp_helptext 'sp_ConvertToChar_IPlyska'
+
+select * from IPlyskaView
+
+select * from #expected
+select * from #actual
+
+--drop procedure Test_spUnitTestClassIPlyska.Setup
+
