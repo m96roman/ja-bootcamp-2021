af508c4d84320b6f8c6d8c7fb844c1de185811eb
tennesseee <antdvgi@gmail.com>
2021-10-18 10:31:58 +0300

    upd

--- Database&SQL/ConvertValues_test.sql
+++ Database&SQL/ConvertValues_test.sql
@@ -10,7 +10,7 @@ CREATE OR ALTER PROCEDURE Test_spDovhanychConvert.[Setup]
 AS
 BEGIN 
 	EXEC tSQLt.FakeTable 'CarModel', @identity=1
-	INSERT INTO CarModel(BrandId) VALUES ('10 as string')
+	INSERT INTO CarModel(BrandId) VALUES (10)
 END
 GO
 
@@ -20,11 +20,11 @@ BEGIN
 	CREATE TABLE #BrandIdExpected (BrandId VARCHAR(50))
 	INSERT INTO #BrandIdExpected (BrandId)
 	VALUES ('10 as string')
-	
-	EXEC [dbo].spDovhanych_ConvertValues(10)
 
-	EXEC tSQLt.AssertEqualsTable #BrandIdExpected, CarModel
-	
+	CREATE TABLE #BrandIdExpected1 (BrandId VARCHAR(50))
+	INSERT INTO #BrandIdExpected1 EXEC [dbo].spDovhanych_ConvertValues
+
+	EXEC tSQLt.AssertEqualsTable #BrandIdExpected, #BrandIdExpected1
 END
 Go
 
