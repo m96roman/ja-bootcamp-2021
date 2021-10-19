e79f3f38d7f8fdd06fb3730aeec9a1bb5157a58f
Kate Fedak <katefedakk@gmail.com>
2021-10-19 23:58:55 +0300

    add a few more test

--- Testing/TDD/TDD-KFedak/UnitTestKFedak/PhoneUnitTest/UnitTest1.cs
+++ Testing/TDD/TDD-KFedak/UnitTestKFedak/PhoneUnitTest/UnitTest1.cs
@@ -29,6 +29,35 @@ namespace PhoneUnitTest
         }
 
         [Test]
+        public void CallAmbulanceLess5Message()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(4, "5600", logger);
+
+            //assert
+            var ex = Assert.Throws<BatteryIsDeadException>(() => phone.CallAmbulance());
+            var message = logger.message.Find(m => m == $"Phone failed to call an ambulance: {phone.PhoneName}");
+            Assert.That(ex.Message, Is.EqualTo(message));
+        }
+
+        [Test]
+        public void CallAmbulanceMore5Message()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(9, "5600", logger);
+            phone.CallAmbulance();
+
+            //assert
+            Assert.IsTrue(logger.message.Contains($"Calling an ambulance from {phone.PhoneName}, remaining charge: {phone.BatteryLevel}%"));
+        }
+
+        [Test]
         public void CheckChargeMessage()
         {
             //arrange
@@ -81,6 +110,74 @@ namespace PhoneUnitTest
             Assert.That(ex.Message, Is.EqualTo("Invalid value!"));
         }
 
+        [Test]
+        public void ChargeValue()
+        {
+            //arrange
+            var logger = InitLogger();
 
+            //act
+            Nokia phone = new Nokia(3, "5600", logger);
+            phone.Charge();
+
+            //assert
+            Assert.That(phone.BatteryLevel, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ChargeABitValue()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(19, "5600", logger);
+            phone.ChargeABit();
+
+            //assert
+            Assert.That(phone.BatteryLevel, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void PrayForABatteryValue()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(19, "5600", logger);
+            phone.PrayForBattery();
+
+            //assert
+            Assert.That(phone.BatteryLevel, Is.EqualTo(27));
+        }
+
+        [Test]
+        public void CallAmbulanceMoreThan5Value()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(19, "5600", logger);
+            phone.CallAmbulance();
+
+            //assert
+            Assert.That(phone.BatteryLevel, Is.EqualTo(14));
+        }
+
+        [Test]
+        public void CallAmbulanceLessThan5Value()
+        {
+            //arrange
+            var logger = InitLogger();
+
+            //act
+            Nokia phone = new Nokia(4, "5600", logger);
+
+            //assert
+            var ex = Assert.Throws<BatteryIsDeadException>(() => phone.CallAmbulance());
+            Assert.That(ex.Telephone.BatteryLevel, Is.EqualTo(0));
+        }
     }
 }
