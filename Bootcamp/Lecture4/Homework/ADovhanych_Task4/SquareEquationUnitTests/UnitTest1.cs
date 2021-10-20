63d6fe9cccca2a7ba17d640e3a0b8e53a2267199
tennesseee <antdvgi@gmail.com>
2021-10-20 18:03:44 +0300

    Day2 Initial setup

---
+++ Bootcamp/Lecture4/Homework/ADovhanych_Task4/SquareEquationUnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace SquareEquationUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckForValidInputValues()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DeterminantCheckForValidOutput()
+        {
+
+        }
+    }
+}
---
+++ Bootcamp/Lecture4/Homework/ADovhanych_Task4/TDDSquareEquation/InputData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDSquareEquation
+{
+    class InputData
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+    }
+}
