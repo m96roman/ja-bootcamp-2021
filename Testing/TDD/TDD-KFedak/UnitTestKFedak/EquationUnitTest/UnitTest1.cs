bc899e82802be267400f809d33f91dc2e73bdaf7
KateFedak <katefedakk@gmail.com>
2021-10-21 10:08:02 +0300

    add UnitTest FOR equation

--- Testing/TDD/TDD-KFedak/UnitTestKFedak/EquationUnitTest/UnitTest1.cs
+++ Testing/TDD/TDD-KFedak/UnitTestKFedak/EquationUnitTest/UnitTest1.cs
@@ -40,13 +40,39 @@ namespace EquationUnitTest
 
         [Test]
         [TestCase(1, 2, 5)]
-        public void TheresNoSolution(double a, double b, double c)
+        public void EquationHasNotSolution(double a, double b, double c)
         {
             //arrange
             QuadraticFunction quadraticFunction = new QuadraticFunction(a, b, c);
 
             //act&&assert
             Assert.That(() => quadraticFunction.Slove(), Throws.TypeOf<NoSolution>());
+        }
+
+        [Test]
+        [TestCase(1, 2, 5)]
+        public void EquationHasNotSolutionMessage(double a, double b, double c)
+        {
+            //arrange
+            QuadraticFunction quadraticFunction = new QuadraticFunction(a, b, c);
+
+            //act&&assert
+            var ex = Assert.Throws<NoSolution>(() => quadraticFunction.Slove());
+            Assert.That(ex.Message, Is.EqualTo("Determinate < 0.\n No solution"));
+        }
+
+        [Test]
+        [TestCase (1,2,5)]
+        public void CorrectValueOfDelta(double a, double b, double c)
+        {
+            //arrange
+            QuadraticFunction quadraticFunction = new QuadraticFunction(a, b, c);
+
+            //act
+            var actualDelta = quadraticFunction.CalculateDelta();
+            var expectedDelta = System.Math.Pow(b, 2) - 4 * a * c;
+
+            Assert.AreEqual(actualDelta, expectedDelta);
         }
     }
 }
