7c7d1d81a3e7530636fd2d27e8f1f6df66cefee5
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-21 18:23:24 +0300

    Update SquareEquationSolverTest.cs

--- Testing/TDD/TDD-IPlyska/TDD_IPlyska/TDD_IPlyska/SquareEquationSolverTest.cs
+++ Testing/TDD/TDD-IPlyska/TDD_IPlyska/TDD_IPlyska/SquareEquationSolverTest.cs
@@ -7,35 +7,98 @@ namespace TDD_IPlyska
     [TestFixture]
     public class SquareEquationSolverTest
     {
+        SquareEquationSolver equation;
         [SetUp]
         public void Setup()
         {
+            equation = new SquareEquationSolver();
         }
 
         [TestCase(0, 0, 0)]
+        [TestCase(0, 0, 2)]
+        [TestCase(2, 0, 0)]
         public void Solve_GivenIvalidData_ShouldThrowExeption(double a, double b, double c)
         {
-            //assert
-            SquareEquationSolver equation = new SquareEquationSolver();
+            //arrange
             //act assert
             Assert.That(() => equation.Solve(a, b, c), Throws.TypeOf<ArgumentException>().With.Property("Message").EqualTo("Arguments can not be equals zero."));
         }
 
-        [TestCase(0, 2, 3)]
-        [TestCase(2, 2, 0)]
-        public void Solve_Given_A_Zero_Should_Have_One_Root(double a, double b, double c)
+        [TestCase(0, 3, 0, 0)]
+        [TestCase(2, 0, 0, 0)]
+        public void Solve_Given_Two_Zero_Values(double a, double b, double c, double expectedValue)
         {
+            EquationRoots roots = equation.Solve(a, b, c);
             //assert
-            SquareEquationSolver equation = new SquareEquationSolver();
-
+            Assert.That(roots.R1, Is.EqualTo(expectedValue));
+            Assert.That(roots.R2, Is.EqualTo(expectedValue));
         }
 
-        [Test]
-        public void Solve_Check_When_Discriminant_Equal_Zero(double a, double b, double c)
+        [TestCase(0, 2, 3, -1.5)]
+        [TestCase(0, 2, -4, 2)]
+        [TestCase(0, -20, -4, -0.2)]
+        [TestCase(0, -20.3, -4.9, -0.2413793103)]
+        public void Solve_Given_A_Zero_Should_Have_One_Root(double a, double b, double c, double expectedValue)
         {
+            //arrange
+            //act
+            EquationRoots roots = equation.Solve(a, b, c);
             //assert
-            SquareEquationSolver equation = new SquareEquationSolver();
-            Assert.Pass();
+            Assert.That(roots.R1, Is.EqualTo(expectedValue));
+            Assert.That(roots.R2, Is.EqualTo(null));
+        }
+
+        [TestCase(-2, -4.5, 0, -2.25)]
+        [TestCase(-20.08, -4.58, 0, -0.2280876494)]
+        [TestCase(31.4, -41.5, 0, 1.321656051)]
+        public void Solve_Given_C_Zero_Have_Two_Roots(double a, double b, double c, double expectedValue)
+        {
+            //act
+            EquationRoots roots = equation.Solve(a, b, c);
+            //assert
+            Assert.That(roots.R2, Is.EqualTo(expectedValue));
+            Assert.That(roots.R1, Is.EqualTo(0));
+        }
+
+        [TestCase(-4.67, 0, 22, 2.1704655655)]
+        [TestCase(9.67, 0, -2.99, 0.5560608985)]
+        public void Solve_Given_B_Zero_One_Way_With_Roots(double a, double b, double c, double expectedValue)
+        {
+            //act
+            EquationRoots roots = equation.Solve(a, b, c);
+            //assert
+            Assert.That(roots.R1, Is.EqualTo(expectedValue));
+            Assert.That(roots.R2, Is.EqualTo(-expectedValue));
+        }
+
+        [TestCase(-4, 0, -5)]
+        [TestCase(44, 0, 15)]
+        [TestCase(0.4564, 0, 0.3335)]
+        public void Solve_Given_B_Zero_One_Way_With_NoRoots_Should_Throw_Exception(double a, double b, double c)
+        {
+            //act
+            //arrange
+             Assert.That(() => equation.Solve(a, b, c), Throws.TypeOf<NoRootsException>());
+        }
+
+        [TestCase(2.2, 6, 3, -3.1917746553, -10.0082253447)]
+        [TestCase(-5.6, 2.22, 1.24, -9.7965730599, 22.2285730599)]
+        public void Solve_If_Discriminant_More_Than_Zero(double a, double b, double c, double r1, double r2)
+        {
+            //act
+            EquationRoots roots = equation.Solve(a, b, c);
+            //assert
+            Assert.That(roots.R1, Is.EqualTo(r1));
+            Assert.That(roots.R2, Is.EqualTo(r2));
+        }
+
+        [TestCase(1.1, 5, 77)]
+        [TestCase(10, 0.99999, 7)]
+        [TestCase(0.55, 2.4, 3.3)]
+        public void Solve_If_Discriminanrt_Less_Than_Zero_Shold_Throw_Exception(double a, double b, double c)
+        {
+            //arrange
+            Assert.That(() => equation.Solve(a, b, c), Throws.TypeOf<NoRootsException>());
         }
 
     }
@@ -44,11 +107,23 @@ namespace TDD_IPlyska
     {
         internal EquationRoots Solve(double a, double b, double c)
         {
+            bool status = false;
             EquationRoots equation;
             ParametersValidation(a, b, c);
+            if (ParametersValidationForZero(a, b, c))
+            {
+                return new EquationRoots() { R1 = 0, R2 = 0 };
+            }
+            equation = OneParameterIsZero(a, b, c, out status);
 
-
-            return new EquationRoots();
+            if (status)
+            {
+                return equation;
+            }
+            else
+            {
+                return FindDiscriminant(a, b, c);
+            }
         }
 
         private static void ParametersValidation(double a, double b, double c)
@@ -58,7 +133,60 @@ namespace TDD_IPlyska
                 throw new ArgumentException("Arguments can not be equals zero.");
             }
 
-           
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Arguments can not be equals zero.");
+            }
+
+        }
+
+        private static bool ParametersValidationForZero(double a, double b, double c)
+        {
+            bool status = false;
+
+            if(a == 0 && c == 0)
+            {
+                status = true; 
+            }
+
+            if (b == 0 && c == 0)
+            {
+                status = true;
+            }
+
+            return status;
+        }
+
+
+        private static EquationRoots FindDiscriminant(double a, double b, double c)
+        {
+            EquationRoots equation;
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                throw new NoRootsException();
+            }
+
+            if (discriminant > 0)
+            {
+                return equation = new EquationRoots()
+                {
+                    R1 = Math.Round((-b + Math.Sqrt(discriminant)) / 2 * a, 10),
+                    R2 = Math.Round((-b - Math.Sqrt(discriminant)) / 2 * a, 10),
+                };
+            }
+
+            if (discriminant == 0)
+            {
+                return equation = new EquationRoots()
+                {
+                    R1 = Math.Round(-b / 2 * a, 10),
+                    R2 = Math.Round(-b / 2 * a, 10),
+                };
+            }
+
+            return new EquationRoots();
         }
 
         private static EquationRoots OneParameterIsZero(double a, double b, double c, out bool status)
@@ -66,24 +194,29 @@ namespace TDD_IPlyska
             if (a == 0 && b != 0 && c != 0)
             {
                 status = true;
-                return new EquationRoots() { R1 = -c / b , R2 = null};
+                return new EquationRoots() { R1 = Math.Round(-c / b, 10) , R2 = null};
             }
 
             if (a != 0 && b == 0 && c != 0)
             {
                 status = true;
 
-                return new EquationRoots() { R1 = Math.Round(Math.Sqrt(-c / a), 5), R2 = null };
+                if (-c / a > 0)
+                {
+                    return new EquationRoots() { R1 = Math.Round(Math.Sqrt(-c / a), 10), R2 = -Math.Round(Math.Sqrt(-c / a), 10) };
+                }
+
+                 throw new NoRootsException();  
             }
 
             if (a != 0 && b != 0 && c == 0)
             {
                 status = true;
-                return new EquationRoots() { R1 = 0, R2 = Math.Round(-b / a, 5) };
-
+                return new EquationRoots() { R1 = 0, R2 = Math.Round(-b / a, 10) };
             }
+
             status = false;
-            return new EquationRoots();
+            return new EquationRoots() {R1 = null, R2 = null};
         }
 
     }
@@ -112,12 +245,5 @@ namespace TDD_IPlyska
         {
         }
 
-        public NoRootsException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected NoRootsException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
     }
 }
