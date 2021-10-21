61b6810b7b9f3fc737619ffb3a2eb486c361bcd0
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-10-21 09:57:12 +0300

    Fixed logical in GetSolution

--- Testing/TDD/QuadraticSolver/Solver.cs
+++ Testing/TDD/QuadraticSolver/Solver.cs
@@ -12,16 +12,23 @@ namespace QuadraticSolver
             }
 
             double D = GetDiscriminant(a, b, c);
-            if (D >= 0)
-            {
-                double root1 = ((-b + Math.Sqrt(D)) / (2 * a));
-                double root2 = ((-b - Math.Sqrt(D)) / (2 * a));
 
-                return new Solution(D > 0 ? TypesOfSolution.TwoRealRoots : TypesOfSolution.OneRoot,
-                    root1, root2);
+            if (D < 0)
+            {
+                throw new NoRootsException($"Equation dont have real roots");
             }
 
-            throw new NoRootsException($"Equation dont have real roots");
+            double root1 = ((-b + Math.Sqrt(D)) / (2 * a));
+            double root2 = ((-b - Math.Sqrt(D)) / (2 * a));
+
+            TypesOfSolution type = TypesOfSolution.OneRoot;
+
+            if (D > 0)
+            {
+                type = TypesOfSolution.TwoRealRoots;
+            }
+
+            return new Solution(type, root1, root2);
         }
 
         internal static double GetDiscriminant(double a, double b, double c)
