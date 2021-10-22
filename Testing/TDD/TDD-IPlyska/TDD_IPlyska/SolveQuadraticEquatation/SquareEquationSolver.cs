999cc4450629206bdba161a8bff644bdca15c5d9
Yuriy Plyska <Yuriyply@gmail.com>
2021-10-22 11:18:35 +0300

    done

---
+++ Testing/TDD/TDD-IPlyska/TDD_IPlyska/SolveQuadraticEquatation/SquareEquationSolver.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveQuadraticEquatation
+{
+    internal class SquareEquationSolver
+    {
+        internal EquationRoots Solve(double a, double b, double c)
+        {
+            bool status = false;
+            EquationRoots equation;
+            ParametersValidation(a, b, c);
+
+            if (ParametersValidationForZero(a, b, c))
+            {
+                return new EquationRoots() { R1 = 0, R2 = 0 };
+            }
+            equation = OneParameterIsZero(a, b, c, out status);
+
+            if (status)
+            {
+                return equation;
+            }
+            else
+            {
+                return FindDiscriminant(a, b, c);
+            }
+        }
+
+        private static void ParametersValidation(double a, double b, double c)
+        {
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException("Arguments can not be equals zero.");
+            }
+
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Arguments can not be equals zero.");
+            }
+
+        }
+
+        private static bool ParametersValidationForZero(double a, double b, double c)
+        {
+            bool status = false;
+
+            if (a == 0 && c == 0)
+            {
+                status = true;
+            }
+
+            if (b == 0 && c == 0)
+            {
+                status = true;
+            }
+
+            return status;
+        }
+
+
+        private static EquationRoots FindDiscriminant(double a, double b, double c)
+        {
+            EquationRoots equation;
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                throw new NoRootsException();
+            }
+
+            if (discriminant > 0)
+            {
+                return equation = new EquationRoots()
+                {
+                    R1 = Math.Round((-b + Math.Sqrt(discriminant)) / 2 * a, 10),
+                    R2 = Math.Round((-b - Math.Sqrt(discriminant)) / 2 * a, 10),
+                };
+            }
+
+            if (discriminant == 0)
+            {
+                return equation = new EquationRoots()
+                {
+                    R1 = Math.Round(-b / (2 * a), 10),
+                };
+            }
+
+            return new EquationRoots();
+        }
+
+        private static EquationRoots OneParameterIsZero(double a, double b, double c, out bool status)
+        {
+            if (a == 0 && b != 0 && c != 0)
+            {
+                status = true;
+                return new EquationRoots() { R1 = Math.Round(-c / b, 10), R2 = null };
+            }
+
+            if (a != 0 && b == 0 && c != 0)
+            {
+                status = true;
+
+                if (-c / a > 0)
+                {
+                    return new EquationRoots() { R1 = Math.Round(Math.Sqrt(-c / a), 10), R2 = -Math.Round(Math.Sqrt(-c / a), 10) };
+                }
+
+                throw new NoRootsException();
+            }
+
+            if (a != 0 && b != 0 && c == 0)
+            {
+                status = true;
+                return new EquationRoots() { R1 = 0, R2 = Math.Round(-b / a, 10) };
+            }
+
+            status = false;
+            return new EquationRoots() { R1 = null, R2 = null };
+        }
+    }
+}
--- Testing/TDD/TDD-IPlyska/TDD_IPlyska/TDD_IPlyska/SquareEquationSolverTest.cs
+++ Testing/TDD/TDD-IPlyska/TDD_IPlyska/TDD_IPlyska/SquareEquationSolverTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using SolveQuadraticEquatation;
 using System;
 using System.Runtime.Serialization;
 
@@ -16,7 +17,6 @@ namespace TDD_IPlyska
 
         [TestCase(0, 0, 0)]
         [TestCase(0, 0, 2)]
-        [TestCase(2, 0, 0)]
         public void Solve_GivenIvalidData_ShouldThrowExeption(double a, double b, double c)
         {
             //arrange
@@ -26,7 +26,7 @@ namespace TDD_IPlyska
 
         [TestCase(0, 3, 0, 0)]
         [TestCase(2, 0, 0, 0)]
-        public void Solve_Given_Two_Zero_Values(double a, double b, double c, double expectedValue)
+        public void Solve_Given_Two_Zero_Values_Should_Have_One_Root(double a, double b, double c, double expectedValue)
         {
             EquationRoots roots = equation.Solve(a, b, c);
             //assert
@@ -101,149 +101,15 @@ namespace TDD_IPlyska
             Assert.That(() => equation.Solve(a, b, c), Throws.TypeOf<NoRootsException>());
         }
 
-    }
-
-    internal class SquareEquationSolver
-    {
-        internal EquationRoots Solve(double a, double b, double c)
+        [TestCase(2, 4, 2, -1)]
+        [TestCase(2, 8, 8, -2)]
+        [TestCase(5, -10, 5, 1)]
+        public void Solve_If_Discriminant_Equal_Zero(double a, double b, double c, double r1)
         {
-            bool status = false;
-            EquationRoots equation;
-            ParametersValidation(a, b, c);
-            if (ParametersValidationForZero(a, b, c))
-            {
-                return new EquationRoots() { R1 = 0, R2 = 0 };
-            }
-            equation = OneParameterIsZero(a, b, c, out status);
-
-            if (status)
-            {
-                return equation;
-            }
-            else
-            {
-                return FindDiscriminant(a, b, c);
-            }
+            //act
+            EquationRoots roots = equation.Solve(a, b, c);
+            //assert
+            Assert.That(roots.R1, Is.EqualTo(r1));
         }
-
-        private static void ParametersValidation(double a, double b, double c)
-        {
-            if (a == 0 && b == 0 && c == 0)
-            {
-                throw new ArgumentException("Arguments can not be equals zero.");
-            }
-
-            if (a == 0 && b == 0)
-            {
-                throw new ArgumentException("Arguments can not be equals zero.");
-            }
-
-        }
-
-        private static bool ParametersValidationForZero(double a, double b, double c)
-        {
-            bool status = false;
-
-            if(a == 0 && c == 0)
-            {
-                status = true; 
-            }
-
-            if (b == 0 && c == 0)
-            {
-                status = true;
-            }
-
-            return status;
-        }
-
-
-        private static EquationRoots FindDiscriminant(double a, double b, double c)
-        {
-            EquationRoots equation;
-            double discriminant = b * b - 4 * a * c;
-
-            if (discriminant < 0)
-            {
-                throw new NoRootsException();
-            }
-
-            if (discriminant > 0)
-            {
-                return equation = new EquationRoots()
-                {
-                    R1 = Math.Round((-b + Math.Sqrt(discriminant)) / 2 * a, 10),
-                    R2 = Math.Round((-b - Math.Sqrt(discriminant)) / 2 * a, 10),
-                };
-            }
-
-            if (discriminant == 0)
-            {
-                return equation = new EquationRoots()
-                {
-                    R1 = Math.Round(-b / 2 * a, 10),
-                    R2 = Math.Round(-b / 2 * a, 10),
-                };
-            }
-
-            return new EquationRoots();
-        }
-
-        private static EquationRoots OneParameterIsZero(double a, double b, double c, out bool status)
-        {
-            if (a == 0 && b != 0 && c != 0)
-            {
-                status = true;
-                return new EquationRoots() { R1 = Math.Round(-c / b, 10) , R2 = null};
-            }
-
-            if (a != 0 && b == 0 && c != 0)
-            {
-                status = true;
-
-                if (-c / a > 0)
-                {
-                    return new EquationRoots() { R1 = Math.Round(Math.Sqrt(-c / a), 10), R2 = -Math.Round(Math.Sqrt(-c / a), 10) };
-                }
-
-                 throw new NoRootsException();  
-            }
-
-            if (a != 0 && b != 0 && c == 0)
-            {
-                status = true;
-                return new EquationRoots() { R1 = 0, R2 = Math.Round(-b / a, 10) };
-            }
-
-            status = false;
-            return new EquationRoots() {R1 = null, R2 = null};
-        }
-
-    }
-
-    internal struct EquationRoots
-    {
-        public double? R1 { get; set; }
-        public double? R2 { get; set; }
-        public override string ToString() => $"Root #1: {R1}; Root #2: {R2}";
-
-        public EquationRoots(double? r1 , double? r2 )
-        {
-            R1 = r1;
-            R2 = r2;
-        }
-
-    }
-
-    public class NoRootsException : Exception
-    {
-        public NoRootsException()
-        {
-        }
-
-        public NoRootsException(string message) : base(message)
-        {
-        }
-
     }
 }
