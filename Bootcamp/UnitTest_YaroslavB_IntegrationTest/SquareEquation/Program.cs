8e55174ae70bee828863581a856e557d631b35bc
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-10-22 16:00:32 +0300

    Init commit

---
+++ Bootcamp/UnitTest_YaroslavB_IntegrationTest/SquareEquation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SquareEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Determinant
+    {
+
+
+    }
+}
