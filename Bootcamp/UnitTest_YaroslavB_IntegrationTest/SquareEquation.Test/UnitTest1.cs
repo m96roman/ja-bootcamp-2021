9c7eef9502215aad1d001b55334e3ef92878095e
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-10-22 17:52:21 +0300

    Added three test templates for UnitTest

--- Bootcamp/UnitTest_YaroslavB_IntegrationTest/SquareEquation.Test/UnitTest1.cs
+++ Bootcamp/UnitTest_YaroslavB_IntegrationTest/SquareEquation.Test/UnitTest1.cs
@@ -2,17 +2,54 @@ using NUnit.Framework;
 
 namespace SquareEquation.Test
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Discriminant_GivenResult_IsPositive_ShouldReturn_TwoRoots()
         {
+            //arrange
+
+
+            //act
+
+
+            //assert
+            Assert.Fail();
         }
 
         [Test]
-        public void Test1()
+        public void Discriminant_GivenResult_IsZero_ShouldReturn_OneRoot()
         {
-            Assert.Pass();
+            //arrange
+
+
+            //act
+
+
+            //assert
+            Assert.Fail();
         }
+
+        [Test]
+        public void Discriminant_GivenResult_IsNegative_ShouldReturn_NoRoots()
+        {
+            //arrange
+
+
+            //act
+
+
+            //assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GettingRoots_tbd()
+        {
+            //given a-1 b-3 c-2, should return roots: -1, -2
+        }
+
+
     }
 }
--- Bootcamp/UnitTest_YaroslavB_IntegrationTest/SquareEquation/Program.cs
+++ Bootcamp/UnitTest_YaroslavB_IntegrationTest/SquareEquation/Program.cs
@@ -15,4 +15,6 @@ namespace SquareEquation
 
 
     }
+
+    
 }
