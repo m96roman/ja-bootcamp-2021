749d3c3ce957f207afbc7d184d3843eb626a8fe3
KateFedak <katefedakk@gmail.com>
2021-10-22 13:46:59 +0300

    .

---
+++ Testing/UnitTesting/Controller-KFedak/KFedakT8/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFedakT8
+{
+    public class Controller
+    {
+        public string Name { get; set; }
+
+        public ILogger Logger { get; set; }
+
+        [Route(Name = "/controller/get")]
+        public int Get()
+        {
+            return 8;
+        }
+
+        [Route(Name = "/controller/post")]
+        public void Post()
+        {
+           Logger.WriteLine($"Post method execute in {Name} controller");
+        }
+
+        [Route(Name = "/controller/put")]
+        public void Put()
+        {
+            Logger.WriteLine($"Put method execute in {Name} controller");
+        }
+
+        [Route(Name = "/controller/delete")]
+        public void Delete()
+        {
+            Logger.WriteLine($"Delete method execute in {Name} controller");
+        }
+
+    }
+}
