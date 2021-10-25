ca50a91a20de5d168b334a5be0c1fabbd8f0c1af
Nestor Pylat <nestorpylat33@gmail.com>
2021-10-25 13:52:43 +0300

    Moq Lecture 4

--- Testing/Pylat test 4/Lecture4/SquareEquation.UnitTests/CalculationServiceTests.cs
+++ Testing/Pylat test 4/Lecture4/SquareEquation.UnitTests/CalculationServiceTests.cs
@@ -35,23 +35,22 @@ namespace SquareEquation.UnitTests
         [TestCase(1, 16, 1, -0.06274606680622785, -15.937253933193773)]
         [TestCase(1, 2, 1, -1, -1)]
         [TestCase(1, 2, 5, null, null)]
-        public void ResultWriteInFile(double a, double b, double c, double x1, double x2)
+        public void SolveAndSave_SavesOneTime(double a, double b, double c, double x1, double x2)
         {
-
-            //arrange
+            //Arrange
             var path = "s";
             var res = new List<double>() { x1, x2 };
+
             Mock<ISquareEquationService> _squareEquationServiceStub;
             Mock<IResultSaverService> _resultSaverServiceMock;
             CalculationService _calculationService;
 
             Init(out _squareEquationServiceStub, out _resultSaverServiceMock);
             _calculationService = new CalculationService(_squareEquationServiceStub.Object, _resultSaverServiceMock.Object);
-
             _squareEquationServiceStub.Setup((p) => p.Solve(a, b, c)).Returns(res);
 
+            //Act
             _calculationService.SolveAndSave(a, b, c, path);
-
 
             //assert
             _resultSaverServiceMock.Verify(it => it.Save(res, path), Times.Once);
