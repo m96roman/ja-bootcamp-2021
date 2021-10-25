de9ffeeeba911bf002a478a4c3864c8fe328fa66
Nestor Pylat <nestor.pylat@justanswer.com>
2021-10-25 16:44:33 +0300

    Task 1 Lecture 10

---
+++ Bootcamp/LectureF10/Homework/Lecture10/Lecture10/GitService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lecture10
+{
+    public class GitService
+    {
+        public static async Task PrintCount()
+        {
+            var git = new Git();
+            var listOfThreads = new List<Task>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                await Task.Run(() => GitPush(git));
+            }
+
+            Console.WriteLine($"Count: {git.commits.Count}");
+        }
+
+        public static void GitPush(Git git)
+        {
+            for (int i = 0; i < 88; i++)
+            {
+                git.Push(i.ToString());
+            }
+        }
+    }
+}
---
+++ Bootcamp/LectureF10/Homework/Lecture10/Lecture10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Lecture10
+{
+    partial class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await GitService.PrintCount();
+        }
+    }
+}
+
+
