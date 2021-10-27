fa80743a5eef430fb0a85806ac6b046ed11824dd
Nestor Pylat <nestor.pylat@justanswer.com>
2021-10-27 17:14:00 +0300

    Lecture 9 Tasks 1-3

--- Bootcamp/Lecture9/Homework/Pylat Lecture 9!!!/Pylat Lecture 9!!!/Program.cs
+++ Bootcamp/Lecture9/Homework/Pylat Lecture 9!!!/Pylat Lecture 9!!!/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Pylat_Lecture_9___.Task3;
+
 
 namespace Pylat_Lecture_9_Task_1
 {
@@ -7,18 +11,47 @@ namespace Pylat_Lecture_9_Task_1
     {
         static void Main(string[] args)
         {
+            var pathTask1 = @"C:\Users\nestor.pylat\ja-bootcamp-2021\Bootcamp\Lecture9\Homework\Pylat Lecture 9!!!\Pylat Lecture 9!!!\Text.txt";
+            string pathTask3 = $@"C:\Users\nestor.pylat\ja-bootcamp-2021\Bootcamp\Lecture9\Homework\Pylat Lecture 9!!!\Pylat Lecture 9!!!\snapshot.json";
+
             //   Task 1 //////////////
+            /* LineCounter lineCounter = new LineCounter();
+             lineCounter.CreateFileTxt();
+             lineCounter.CountLines(pathTask1);*/
 
-            Task1.CreateFileTxt();
-            Console.WriteLine(Task1.CountLineInFile("PylatLecture9"));
 
-            /*// Task 2 //////////////////////
+            // Task 2 //////////////////////
 
-            Task2.doTask2();*/
+            // Task2.StartProgram();
 
-            /*//   Task 3 ///////////////////////
+            // Task3  ////////////////////
 
-            DoTask3.SimulateSemestr();*/
+            List<Student> students = new List<Student>();
+            Student student = new Student();
+
+            if (File.Exists(pathTask3))
+            {
+                student.JsonDeserialise(pathTask3);
+            }
+            else
+            {
+                Student student1 = new Student() { Name = "r", LecturesAttended = 8 };
+                Student student2 = new Student() { Name = "t", LecturesAttended = 1 };
+                Student student3 = new Student() { Name = "yu", LecturesAttended = 2 };
+                Student student4 = new Student() { Name = "i", LecturesAttended = 3 };
+                Student student5 = new Student() { Name = "o", LecturesAttended = 4 };
+
+                students.Add(student1);
+                students.Add(student2);
+                students.Add(student3);
+                students.Add(student4);
+                students.Add(student5);
+
+                student.SemesterEmulation(students);
+            }
+            // Task4 /////////////////////////
+
+
 
         }
     }
