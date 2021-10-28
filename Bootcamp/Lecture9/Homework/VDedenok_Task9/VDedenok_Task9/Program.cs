32a9f39453f70526fa18b5b93851cd77759433c6
Viktoriia Dedenok <afrovika@gmail.com>
2021-10-28 16:31:07 +0300

    lock task

---
+++ Bootcamp/Lecture9/Homework/VDedenok_Task9/VDedenok_Task9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace VDedenok_Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Task1-----");
+            using (StreamWriter sw = new StreamWriter("TextToCountLinesFrom.txt"))
+            {
+                sw.WriteLine("This is line 1");
+                sw.WriteLine("This is line 2");
+                sw.WriteLine("This is line 3");
+                sw.WriteLine("This is line 4");
+                sw.WriteLine("This is line 5");
+            }
+
+            int linesCount = File.ReadAllLines("TextToCountLinesFrom.txt").Length;
+            Console.WriteLine($"Number of lines in file is {linesCount}.");
+
+            Console.WriteLine("------Task2------");
+            string newline = "";
+            int counter = 0;
+            int indexOfFile = 0;
+            string resulterText = "";
+            string p = Directory.GetCurrentDirectory();
+            string newFullPath = "";
+          
+            do
+            {
+                newline = Console.ReadLine();
+                resulterText += newline;
+                ++counter;
+               
+                if (counter == 2 )
+                {
+                    ++indexOfFile;
+                    newFullPath = @$"{p}\inputFromVim{indexOfFile}.txt";
+                    File.AppendAllText(newFullPath, resulterText);
+                    resulterText = "";
+                    counter = 0;
+                }
+            } 
+            while (newline != "st");
+
+            if (File.Exists(newFullPath))
+            {
+                Console.WriteLine("Last file exists.");
+            }
+
+
+            string[] files = Directory.GetFiles(@$"{p}", "*inputFromVim");
+          
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
+/*
+            * 2. Write a program which reads input from Console infinitely long, with minor restrictions:
+   - When user entered 8 lines of text you should write them in the file inputFromVIM{indexOfFile}.txt
+   - For the next 8 lines write them in the next file inputFromVIM{indexOfFile + 1}.txt and so on.
+BUT when user enters a stop word "LET ME OUT"(or anything else you like) you should do the following:
+   - Make sure that last file inputFromVim{indexOfFile}.txt is created
+   - Move all the created files during the user session to folder Session_{DateTime}
+            */
---
+++ Bootcamp/LectureF10/Homework/VDedenok-task10/VDedenok-Task10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+
+namespace VDedenok_Task10
+{
+    class Program
+    {
+        static List<string> commits = new List<string>();
+
+        class Git
+        {
+            private static object _lock = new object();
+
+            public void Push(string message)
+            {
+                lock (_lock)
+                {
+                    commits.Add(message);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                tasks.Add(Task.Run(() => dailyCommits()));
+                Console.WriteLine();
+            }
+
+
+            static void dailyCommits()
+            {
+                Git gitUser = new Git();
+         
+                for (int i = 0; i < 88; i++)
+                {
+                    gitUser.Push($"Commit # {i}.");
+                }
+             
+            }
+
+            Task.WaitAll(tasks.ToArray());
+          
+            Console.WriteLine(commits.Count);
+        }
+    }
+}
