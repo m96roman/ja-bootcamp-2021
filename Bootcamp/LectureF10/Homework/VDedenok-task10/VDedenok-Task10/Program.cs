190287ad8971a0c7796d3168e6253cdb8fa117d2
Viktoriia Dedenok <viktoriia.dedenok@justanswer.com>
2021-10-29 16:02:51 +0300

    VDedenok_Task10

--- Bootcamp/LectureF10/Homework/VDedenok-task10/VDedenok-Task10/Program.cs
+++ Bootcamp/LectureF10/Homework/VDedenok-task10/VDedenok-Task10/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Linq;
-using System.Threading;
+using System.Net.Http;
+using System.IO;
 
 namespace VDedenok_Task10
 {
@@ -10,44 +10,49 @@ namespace VDedenok_Task10
     {
         static List<string> commits = new List<string>();
 
-        class Git
+        static async Task Main(string[] args)
         {
-            private static object _lock = new object();
-
-            public void Push(string message)
-            {
-                lock (_lock)
-                {
-                    commits.Add(message);
-                }
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            List<Task> tasks = new List<Task>();
-
-            for (int i = 0; i < 8; i++)
-            {
-                tasks.Add(Task.Run(() => dailyCommits()));
-                Console.WriteLine();
-            }
-
-
-            static void dailyCommits()
-            {
-                Git gitUser = new Git();
-         
-                for (int i = 0; i < 88; i++)
-                {
-                    gitUser.Push($"Commit # {i}.");
-                }
-             
-            }
-
-            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("-----Task1-----");
           
-            Console.WriteLine(commits.Count);
+            List<Task> users = new List<Task>();
+
+            StartWork workingDay = new StartWork();
+          
+            await workingDay.Start(users, commits);
+      
+            Task.WaitAll(users.ToArray());
+
+            Console.WriteLine($"Total count of 88 commits, done by 8 users is {commits.Count}.");
+
+           
+            Console.WriteLine("-----Task2-----");
+            bool exists = false;
+            using var client = new HttpClient();
+            string content = "";
+            string fullPath = @$"{Directory.GetCurrentDirectory()}\Relativity.txt";
+
+            while (!exists)
+            {
+                try
+                {
+                    content = await client.GetStringAsync("https://www.gutenberg.org/files/30155/30155-0.txt");
+                    exists = true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            File.WriteAllText(fullPath, content);
+
+            string textToWords = File.ReadAllText(fullPath).ToLower();
+            char[] charSplit = new char[] { '\n', '\r', ' ', '\"', '/', '\\', '*', '!', '?', '\t', '-', '+', '.', ',', '$', '(', ')', '{', '}', '@', '#', '%', '^', '&', '<', '>', '`' };
+            string[] splitedText = textToWords.Split(charSplit, StringSplitOptions.RemoveEmptyEntries);
+
+            RelativityTheory theory = new RelativityTheory();
+
+            await theory.DoAllAnalize(splitedText);
         }
     }
 }
---
+++ Bootcamp/LectureF10/Homework/VDedenok-task10/VDedenok-Task10/StartWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VDedenok_Task10
+{
+    class StartWork
+    {
+        public async Task Start(List<Task> users, List<string> commits)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Git gitUser = new Git();
+
+                users.Add(Task.Run(() =>
+                {
+                    for (int i = 0; i < 88; i++)
+                    {
+                        gitUser.Push($"Commit # {i}.", commits);
+                    }
+                }
+              ));
+            }
+        }
+    }
+}
