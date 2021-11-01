9a5aa45e9eecd055a233ecddad160416726cc03e
Martin Roshko <martin.roshko1@gmail.com>
2021-11-01 18:01:30 +0200

    Add Homework

---
+++ MVC/WEB-TASK1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_TASK1.Models;
+
+namespace WEB_TASK1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private static List<UserModel> UsersList = new List<UserModel>(){
+                    new UserModel{UserName = "Jhon", UserSurname = "Doe" },
+                     new UserModel{UserName = "Martin", UserSurname = "Roshko" },
+                };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       
+        public ViewResult Index()
+        {
+            var user = new UserModel();
+            return View(user);
+        }
+
+        public ViewResult Users()
+        {
+            return View(UsersList);
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(UserModel userModel)
+        {
+            UsersList.Add(userModel);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(int? id) 
+        {
+            UsersList.Remove(UsersList.Where(u => u.UserId == id).FirstOrDefault());           
+            return RedirectToAction("Users");
+        }
+        /*  [HttpPost]
+          public ActionResult Users(UserViewModel user)
+          {
+              UsersList.Add(user);
+
+              return View(user);
+          }*/
+
+       /* [HttpGet]
+        public ViewResult Users()
+        {
+            return View(UsersList);
+        }*/
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC/WEB-TASK1/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_TASK1.Models
+{
+    public class UserModel        
+    {
+        private static int Iterator { get; set; } = default;
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserSurname { get; set; }
+
+        public UserModel() : base()
+        {
+            UserId = Iterator;
+
+            Iterator++;
+        }
+
+    }
+}
