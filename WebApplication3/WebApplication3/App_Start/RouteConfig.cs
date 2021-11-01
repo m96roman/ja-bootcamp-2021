77d219b57bf23f5ccc1a2ed0b538a04ed1242f05
Yevhen Shyptur <eugeni.shyptur@gmail.com>
2021-11-01 18:07:55 +0200

    HW MVC 1

---
+++ WebApplication3/WebApplication3/App_Start/RouteConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "IncorrectValue",
+               url: "Value",
+               defaults: new { controller = "Value", action = "IncorrectValue" }
+           );
+
+            routes.MapRoute(
+              name: "CorrectValue",
+              url: "Value/{id}",
+              defaults: new { controller = "Value", action = "Show", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+            name: "ValueAllRoute",
+            url: "Value/Show/{id}",
+            defaults: new { controller = "Value", action = "Show", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Controllers/ValueController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ValueController : Controller
+    {
+       public static List<Person> peopleList = new List<Person>() { new Person { Id = 123, Name = "Yevhen", Message = "Expected message: 123" } };
+
+
+        // GET: Value
+
+      
+        public ActionResult ShowPersons()
+        {
+
+            return View(peopleList);
+        }
+        public ActionResult Show()
+        {
+            return View(123);
+        }
+        public ActionResult IncorrectValue()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View(new Person());
+        }
+        [HttpPost]
+        public void Addnew(Person person)
+        {
+            peopleList.Add(person);
+          
+        }
+    }
+}
