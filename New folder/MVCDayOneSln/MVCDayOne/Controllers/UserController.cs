7a5eb84100f213be9d6c1c04c617907dbec539fd
tennesseee <antdvgi@gmail.com>
2021-11-02 18:26:13 +0200

    Day2

--- New folder/MVCDayOneSln/MVCDayOne/Controllers/UserController.cs
+++ New folder/MVCDayOneSln/MVCDayOne/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCDayOne.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MVCDayOne.Controllers
 {
@@ -28,10 +29,40 @@ namespace MVCDayOne.Controllers
             return View(Users);
         }
 
-        public ActionResult DeleteUser()
+        public ActionResult DeleteUser(UserModel user)
         {
-            //Users.Remove(Users.Where());
+            Users.Remove(user);
             return RedirectToAction("ShowUsers");
+        }
+
+        public ActionResult SearchUser()
+        {
+            return View(Users);
+        }
+
+        [HttpPost]
+        public JsonResult SearchingData(string SearchBy, string SearchValue)
+        {
+            List<UserModel> userModel = new List<UserModel>();
+
+            if (SearchBy == "FirstName")
+            {
+                try
+                {
+                    string FirstName = SearchValue;
+                    userModel = Users.Where(x => x.FirstName == FirstName).ToList();
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine($"{SearchValue} is incorrect");
+                }
+                return Json(userModel, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                userModel = Users.Where(x => x.LastName.Contains(SearchValue)).ToList();
+                return Json(userModel, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
