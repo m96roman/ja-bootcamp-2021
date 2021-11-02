dfda2c0dc912acee08e315d4852ae00bde1e1537
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-02 18:17:45 +0200

    Lecture 2

---
+++ MVC/Lecture 2/Lecture 2 MVC/Lecture 2 MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lecture_2_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "SearchText",
+                url: "{controller}/{action}/{searchText}",
+                defaults: new { controller = "User", action = "FilterUsers", searchText = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            
+        }
+    }
+}
---
+++ MVC/Lecture 2/Lecture 2 MVC/Lecture 2 MVC/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Lecture_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture_2.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<User> listOfUsers = new List<User>() {
+           new User() { Id = 1, Name = $"User1", LastName = $"LastName1" },
+           new User() { Id = 2, Name = $"User2", LastName = $"LastName2" }
+       };
+
+        // GET: User
+        public ActionResult User()
+        {
+            return View(listOfUsers);
+        }
+
+        public JsonResult FilterUsers(string searchText = "")
+        {
+            var filtredUsers = listOfUsers.Where(user => user.Name.Contains(searchText)).ToList();
+
+            return Json(filtredUsers, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
