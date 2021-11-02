ac27d3f9e63284bff223c2a43fe7c7c18ee7e6e8
tennesseee <antdvgi@gmail.com>
2021-11-02 12:33:25 +0200

    First day task, without edit/delete

---
+++ New folder/MVCDayOneSln/MVCDayOne/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDayOne.Models;
+using System.Collections.Generic;
+
+namespace MVCDayOne.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<UserModel> Users = new List<UserModel>() {
+            new UserModel {FirstName = "Piter", LastName = "Pan"} }; 
+
+        public ActionResult User()
+        {
+            UserModel users = new UserModel();
+            return View(users);
+        }
+        
+        [HttpPost]
+        public ActionResult CreateUser(UserModel users)
+        {
+            Users.Add(users);
+            return RedirectToAction("ShowUsers");
+        }
+
+        [HttpGet]
+        public ActionResult ShowUsers()
+        {
+            return View(Users);
+        }
+
+        public ActionResult DeleteUser()
+        {
+            //Users.Remove(Users.Where());
+            return RedirectToAction("ShowUsers");
+        }
+    }
+}
---
+++ New folder/MVCDayOneSln/MVCDayOne/Controllers/ValueController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDayOne.Controllers
+{
+    public class ValueController : Controller
+    {
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+        [Route("/111")]
+        public ActionResult WithoutValue()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/Value/{id}")]
+        public ActionResult ValueId(int id)
+        {
+            return View(id);
+        }
+    }
+}
