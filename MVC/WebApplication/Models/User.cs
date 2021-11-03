a5fcfd476a76a6a958a8f3d2d8b3289d30738941
CyberVS <39273097+CyberVS@users.noreply.github.com>
2021-11-03 09:12:51 +0200

    MVC VSkorynovych Lecture 1-2

---
+++ MVC/WebApplication/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
