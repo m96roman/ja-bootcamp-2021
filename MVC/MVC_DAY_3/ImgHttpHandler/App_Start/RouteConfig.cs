6dceba38b01c165d34168d0d7aa3d59d178fce7c
KateFedak <katefedakk@gmail.com>
2021-11-04 15:17:36 +0200

    httphandler  custom model binder

---
+++ MVC/MVC_DAY_3/ImgHttpHandler/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using ImgHttpHandler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ImgHttpHandler
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           routes.IgnoreRoute("{file}.jpg");
+            routes.IgnoreRoute("{file}.gif");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+        class CustomRouteHandler : IRouteHandler
+        {
+            public IHttpHandler GetHttpHandler(RequestContext requestContext)
+            {
+                return new ImageHandler();
+            }
+        }
+    }
+}
---
+++ MVC/MVC_DAY_3/ImgHttpHandler/Models/ImageHandler .cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImgHttpHandler.Models
+{
+    public class ImageHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            string imageURL = null;
+            var nameOfFile = request.FilePath.Replace("/", "");
+            var listOfFile = Directory.GetFiles(request.PhysicalApplicationPath, $"*{request.CurrentExecutionFilePathExtension}");
+            foreach (string file in listOfFile)
+            {
+                if (Path.GetFileName(file)==nameOfFile)
+                {
+                    imageURL = request.PhysicalPath;
+                }
+                else
+                {
+                    imageURL = context.Server.MapPath("~/images/delete-icon.gif");
+                }
+            }
+            response.ContentType = "image/" + Path.GetExtension(imageURL).ToLower();
+            response.WriteFile(imageURL);
+        }
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
