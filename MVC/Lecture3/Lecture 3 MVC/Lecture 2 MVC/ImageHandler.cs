fdaa97b557fbd164bf3f4ba86a52b2794b62b223
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-04 12:28:52 +0200

    Task 8, 9 Lecture 3

---
+++ MVC/Lecture3/Lecture 3 MVC/Lecture 2 MVC/ImageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SimpleHTTPHanlder
+{
+    public class ImageHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            string imageURL = null;
+            if (request.UrlReferrer != null)
+            {
+                if (String.Compare(request.Url.Host, request.UrlReferrer.Host, true, CultureInfo.InvariantCulture) == 0)
+                {
+                    imageURL = request.PhysicalPath;
+                    if (!File.Exists(imageURL))
+                    {
+                        response.Status = "Image Not Found";
+                        response.StatusCode = 404;
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+            if (imageURL == null)
+            {
+                imageURL = context.Server.MapPath("~/images/notallowed.gif");
+            }
+            response.ContentType = "image/" + Path.GetExtension(imageURL).ToLower();
+            response.WriteFile(imageURL);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MVC/Lecture3/Lecture 3 MVC/Lecture 2 MVC/Models/UserBinder.cs
@@ -0,0 +1,37 @@
+﻿using Lecture_2.Models;
+using System;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture_2_MVC.Models
+{
+    public class UserBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, System.Web.Mvc.ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType == typeof(User))
+            {
+                HttpRequestBase request = controllerContext.HttpContext.Request;
+                int id = Convert.ToInt32(request.Form.Get("Id"));
+                string Name = request.Form.Get("Name");
+                string LastName = request.Form.Get("LastName");
+
+                return new User()
+                {
+                    Id = id,
+                    Name =  Regex.Replace(Name, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled),
+                    LastName = Regex.Replace(LastName, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled),
+                    NameAndLastName = $"{Regex.Replace(Name, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled)}.{Regex.Replace(LastName, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled)}"
+
+                };
+            }
+            else
+            {
+                return base.BindModel(controllerContext, bindingContext);
+            }
+
+
+        }
+    }
+}
