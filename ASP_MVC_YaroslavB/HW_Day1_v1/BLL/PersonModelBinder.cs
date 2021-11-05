8b9541849ce4efc5622273d9c9c70eb0fe501b8c
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-11-05 17:30:30 +0200

    Added HW for Day3 - Implement custom model binder

---
+++ ASP_MVC_YaroslavB/HW_Day1_v1/BLL/PersonModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW_Day1_v1.BLL
+{
+    public class PersonModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpContextBase context = controllerContext.HttpContext;
+            string firstName = context.Request.Form.Get("FirstName");
+            string lastName = context.Request.Form.Get("LastName");
+            string firstNameParsed = new string(firstName.Trim().Where(c => char.IsLetter(c)).ToArray());
+            string lastNameParsed = new string(lastName.Trim().Where(c => char.IsLetter(c)).ToArray());
+            string personName = firstNameParsed + "." + lastNameParsed;
+            var person = new HW_Day1_v1.Models.Person() { Name = personName };
+
+            return person;
+        }
+    }
+}
