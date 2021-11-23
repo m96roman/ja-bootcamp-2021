e96e2304277b697bc3f5d9996ff4ad6c518f974b
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-11-23 16:59:34 +0200

    Done prototype

---
+++ DesignPatterns/Day2/Countries_Prototype/Countries_Prototype/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Countries_Prototype
+{
+    internal class City : ICloneable
+    {
+        public City(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+        public string Name { get; internal set; }
+        public int Population { get; internal set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Population}";
+        }
+    }
+}
--- DesignPatterns/Day2/Countries_Prototype/Countries_Prototype/Program.cs
+++ DesignPatterns/Day2/Countries_Prototype/Countries_Prototype/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Countries_Prototype
 {
@@ -6,7 +6,24 @@ namespace Countries_Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Country country = new Country("123",
+                new List<Region> { new Region("Georgia", 1200) },
+                new List<City> { new City("Fukusima", 1000000) });
+
+            country.PrintInfo();
+
+            System.Console.WriteLine(new string('-', 20));
+
+            var countryClone = (Country)country.Clone();
+
+            countryClone.ChangeCityPopulation("Fukusima", 0);
+            countryClone.RenameRegion("Georgia", "New Georgia");
+
+            countryClone.PrintInfo();
+
+            System.Console.WriteLine(new string('-', 20));
+
+            country.PrintInfo();
         }
     }
 }
