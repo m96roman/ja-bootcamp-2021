cbb0669a69a799dfaec222e38789580d77630b5f
eugen4ik <eugeni.shyptur@gmail.com>
2021-11-23 22:58:26 +0200

    Task 5

---
+++ DesignPatterns/Day2/Task1/Task1/Task5/DB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public sealed class DB
+    {
+        private DB()
+        {
+
+        }
+        private static DB _instance;
+        public static DB GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DB();
+            }
+            return _instance;
+        }
+        public  void Request(string SQLRequest)
+        {
+            Console.WriteLine(SQLRequest);
+        }
+    }
+}
