7485b0d58120f9f21719bfece914f3e07d481f24
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-11-23 17:21:00 +0200

    Singelton done

--- DesignPatterns/Day2/Database_Singelton/Database_Singelton/Program.cs
+++ DesignPatterns/Day2/Database_Singelton/Database_Singelton/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Database_Singelton
 {
@@ -6,7 +7,25 @@ namespace Database_Singelton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread1 = new Thread(() => TestDatabase("select * from a; drop table a"));
+            Thread thread2 = new Thread(() => TestDatabase("select * from b; drop table b"));
+            Thread thread3 = new Thread(() => TestDatabase("select * from a; drop table a"));
+            Thread thread4 = new Thread(() => TestDatabase("select * from b; drop table b"));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            thread4.Join();
+        }
+        public static void TestDatabase(string query)
+        {
+            DatabaseContext databaseContext = DatabaseContext.GetIntance();
+            databaseContext.ExecuteQuery(query);
         }
     }
 }
