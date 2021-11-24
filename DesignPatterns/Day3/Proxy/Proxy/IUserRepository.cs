67580c198f9b1163d9a4f59c1c8e10a6c5ff5ae5
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-11-24 16:10:45 +0200

    Proxy done

---
+++ DesignPatterns/Day3/Proxy/Proxy/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Proxy
+{
+    internal interface IUserRepository
+    {
+        List<string> UsersList();
+    }
+}
---
+++ DesignPatterns/Day3/Proxy/Proxy/UserRepoProxy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Proxy
+{
+    class UserRepoProxy : IUserRepository
+    {
+        private IUserRepository userRepository;
+
+        CacheList<string> cacheList = new CacheList<string>();
+
+        public UserRepoProxy(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public List<string> UsersList()
+        {
+            if (!cacheList.IsCached)
+            {
+                cacheList.SetCacheList(userRepository.UsersList());
+            }
+
+            return cacheList.GetList();
+        }
+    }
+}
