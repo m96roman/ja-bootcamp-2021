73bfa4a1b6d312e5914ce008aefbdfe89ef002d7
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-24 15:49:45 +0200

    Init, Adapter added

---
+++ DesignPatterns/Day3/Adapter/Adapter/MessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    class MessageService 
+    {
+        public void SendMessage(IMessage message)
+        {
+            message.Send();
+        }
+    }
+}
---
+++ DesignPatterns/Day3/Adapter/Adapter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageService message = new MessageService();
+            Email email = new Email();
+
+            message.SendMessage(email);
+
+            Dove dove = new Dove();
+            IMessage doveMessage = new EmailToDoveAdapter(dove);
+
+            message.SendMessage(doveMessage);
+
+        }
+    }
+}
