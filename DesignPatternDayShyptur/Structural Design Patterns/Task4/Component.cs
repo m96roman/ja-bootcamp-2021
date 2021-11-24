93499338d477ab59ecd8c89a013ea9c86625a08c
eugen4ik <eugeni.shyptur@gmail.com>
2021-11-24 23:13:17 +0200

    Task 4

---
+++ DesignPatternDayShyptur/Structural Design Patterns/Task4/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    abstract class Component
+    {
+        public Component() { }
+    
+        public abstract string GetRoot();
+  
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+}
--- DesignPatternDayShyptur/Structural Design Patterns/Task4/Program.cs
+++ DesignPatternDayShyptur/Structural Design Patterns/Task4/Program.cs
@@ -6,7 +6,18 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-           
+            User user = new User();
+            File file = new File();
+
+            Folder folder = new Folder();
+            Folder newFolder = new Folder();
+
+            newFolder.Add(new File());
+            newFolder.Add(new File());
+
+            folder.Add(newFolder);
+            folder.Add(newFolder);
+            user.GetRoot(folder);        
         }
     }
 }
