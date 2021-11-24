acac2780c001ad0fda38e39c99b54601a7c5225c
KateFedak <katefedakk@gmail.com>
2021-11-24 15:12:28 +0200

    Composite

---
+++ DesignPatterns/Day3/StructuralPatterns/Composite/Component .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal abstract class Component
+    {
+        protected readonly string Name;
+
+        public Component(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void Add(Component component) { }
+
+        public virtual void Remove(Component component) { }
+
+        public virtual void PrintTree()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ DesignPatterns/Day3/StructuralPatterns/Composite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Composite
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Component fileSystem = new Folder("File system");
+            Component diskC = new Folder("Start path C:\\..");
+
+            
+            Component pictureComponent = new File("flower.jpg");
+            Component textComponent = new File("text.txt");
+
+            diskC.Add(pictureComponent);
+            diskC.Add(textComponent);
+
+            fileSystem.Add(diskC);
+
+            fileSystem.PrintTree();
+
+            Console.WriteLine("*****************************************");
+            diskC.Remove(textComponent);
+            
+            Component myMusic = new Folder("My music");
+            Component songOne = new File("sun.mp3");
+            Component songTwo = new File("moon.mp3");
+
+            myMusic.Add(songOne);
+            myMusic.Add(songTwo);
+            diskC.Add(myMusic);
+
+            fileSystem.PrintTree();
+        }
+    }
+}
