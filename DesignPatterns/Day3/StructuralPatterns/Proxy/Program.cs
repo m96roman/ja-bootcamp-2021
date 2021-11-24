0d2b44cb1edd8bd665986e9226a2bab372535499
KateFedak <katefedakk@gmail.com>
2021-11-24 17:52:12 +0200

    Proxy

---
+++ DesignPatterns/Day3/StructuralPatterns/Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VodafoneUserRepository vodafoneUser = new VodafoneUserRepository();
+        //   Proxy proxy = new Proxy(vodafoneUser);
+
+         //  proxy.GetUserList().ForEach(l=>Console.WriteLine(l));
+           IUserRepository userRepository = new Proxy(vodafoneUser);
+           userRepository.GetUserList().ForEach(l => Console.WriteLine(l)); ;
+           userRepository.GetUserList().ForEach(l => Console.WriteLine(l)); ;
+           userRepository.GetUserList().ForEach(l => Console.WriteLine(l)); ;
+
+
+
+        }
+    }
+}
---
+++ DesignPatterns/Day3/StructuralPatterns/Proxy/Proxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Proxy:IUserRepository
+    {
+        private IUserRepository userRepository;
+        private List<string> cacheDate= new List<string>();
+        private  DateTime cacheOn;
+
+        public Proxy(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+            cacheOn=DateTime.MinValue;
+        }
+
+        public List<string> GetUserList()
+        {
+            if (userRepository == null)
+                userRepository = new VodafoneUserRepository();
+            if (cacheOn.AddMinutes(1)<DateTime.Now)
+            {
+                cacheDate = userRepository.GetUserList();
+                cacheOn = DateTime.Now;
+                return cacheDate;
+            }
+
+            Console.WriteLine("Proxy");
+            return cacheDate;
+        }
+    }
+}
