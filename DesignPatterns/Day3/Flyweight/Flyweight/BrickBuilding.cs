0d1a5102fc4eedc1406c04d373d066985aee5be2
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-24 17:55:04 +0200

    Flyweight init

---
+++ DesignPatterns/Day3/Flyweight/Flyweight/BrickBuilding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    class BrickBuilding : Buildings
+    {
+        public BrickBuilding()
+        {
+            floors = 5;
+        }
+    }
+}
---
+++ DesignPatterns/Day3/Flyweight/Flyweight/MonolithBuilding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    class MonolithBuilding : Buildings
+    {
+        public MonolithBuilding(int floorCount)
+        {
+            floors = floorCount;
+        }
+    }
+}
