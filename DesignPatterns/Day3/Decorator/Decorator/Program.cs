4fa5f6cd1bc539f0365054d38988723d08f35f1b
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-24 16:29:11 +0200

    Decorator Added

--- DesignPatterns/Day3/Decorator/Decorator/Program.cs
+++ DesignPatterns/Day3/Decorator/Decorator/Program.cs
@@ -6,7 +6,10 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Computer computer = new OfficeModel();
+            computer = new OptionalWifi(computer);
+
+            Console.WriteLine($"{computer.Type}");
         }
     }
 }
