b25116c5731e78035736d814907e12ca832cefcd
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-11-24 15:43:50 +0200

    Done composite

---
+++ DesignPatterns/Day3/Composite/Composite/IComposite.cs
@@ -0,0 +1,19 @@
+﻿namespace Composite
+{
+    public interface IComposite
+    {
+        string Name { get; }
+
+        bool IsDirectory { get; }
+
+        IComposite Parent { get; set; }
+
+        IComposite[] GetChildrens();
+
+        IComposite GetChildWithName(string name);
+
+        void AppendChildren(IComposite composite);
+
+        void RemoveChildren(IComposite composite);
+    }
+}
