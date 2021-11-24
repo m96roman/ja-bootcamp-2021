b5221c2a67f5b4a08b1f13978f05c9f1815bdde1
Martin Roshko <martin.roshko1@gmail.com>
2021-11-24 17:16:05 +0200

    Singleton

---
+++ DesignPatterns/Day2/Singleton/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlDb myDb = MySqlDb.GetInstance();
+
+            myDb.CallMySql();
+        }
+    }
+}
