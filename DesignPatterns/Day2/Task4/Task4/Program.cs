ab91e3ddcfa22bb5456c3c9b8b3dcf5c8e5517c4
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-24 11:04:39 +0200

    Test

--- DesignPatterns/Day2/Task4/Task4/Program.cs
+++ DesignPatterns/Day2/Task4/Task4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task4
 {
@@ -6,7 +7,29 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<City> cities = new List<City>();
+            City city = new City() { Name = "Lviv", Population = 123 };
+            City city1 = new City() { Name = "Uzhorod", Population = 453 };
+            cities.Add(city);
+            cities.Add(city1);
+
+            List<Region> regions = new List<Region>();
+            Region region = new Region() { Name = "Lvivska", Square = 302 };
+            Region region1 = new Region() { Name = "Zakarpatska", Square = 302 };
+            regions.Add(region);
+            regions.Add(region1);
+
+            Country ukraine = new Country("Ukraine", cities, regions);
+
+            ukraine.GetInformation();
+
+
+
+            Country poland = ukraine.CountryClone();
+
+            poland.GetInformation();
+            ukraine.ChangePopulation("Lviv", 2354);
+            poland.ChangePopulation("Lviv", 500);
         }
     }
 }
---
+++ DesignPatterns/Day2/Task4/Task4/Region.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+   public class Region
+    {
+        public string Name { get; set; }
+        public int Square { get; set; }
+    }
+}
