9b408726b1187a654c078108b4a070edc23f8650
KateFedak <katefedakk@gmail.com>
2021-11-24 17:53:55 +0200

    improve proxy

--- DesignPatterns/Day3/StructuralPatterns/Proxy/Program.cs
+++ DesignPatterns/Day3/StructuralPatterns/Proxy/Program.cs
@@ -5,11 +5,8 @@ namespace Proxy
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             VodafoneUserRepository vodafoneUser = new VodafoneUserRepository();
-        //   Proxy proxy = new Proxy(vodafoneUser);
-
-         //  proxy.GetUserList().ForEach(l=>Console.WriteLine(l));
            IUserRepository userRepository = new Proxy(vodafoneUser);
            userRepository.GetUserList().ForEach(l => Console.WriteLine(l)); ;
            userRepository.GetUserList().ForEach(l => Console.WriteLine(l)); ;
