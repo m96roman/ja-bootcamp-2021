9574056d382e56fc78be1fca67edf004c4245f28
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-25 17:28:27 +0200

    Meditor

---
+++ DesignPatterns/Day4/ConsoleApp1/ConsoleApp1/CaptainKyiv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator
+{
+    class CaptainKyiv : Captain
+    {
+        public CaptainKyiv(Operator opr) : base(opr) { }
+
+        public override void SpeakToCaptain(string message)
+        {
+            Console.WriteLine($"Message to another captains from Kyiv Cap {message}");
+        }
+    }
+}
---
+++ DesignPatterns/Day4/ConsoleApp1/ConsoleApp1/CaptainLviv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator
+{
+    class CaptainLviv : Captain
+    {
+        public CaptainLviv(Operator opr) : base(opr) { }
+
+        public override void SpeakToCaptain(string message)
+        {
+            Console.WriteLine($"Message to another captains from Lviv Cap {message}");
+        }
+    }
+}
