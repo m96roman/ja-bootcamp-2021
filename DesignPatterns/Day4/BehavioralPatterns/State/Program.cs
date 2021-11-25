9a55382e1a08bb29b4ddd32c34de22a9016aad3f
KateFedak <katefedakk@gmail.com>
2021-11-25 16:02:09 +0200

    State

---
+++ DesignPatterns/Day4/BehavioralPatterns/State/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sportsman sportsman = new Sportsman(new RunSportsmanState());
+            sportsman.IncreaseExerciseIntensity();
+            sportsman.Relax();
+            sportsman.Relax();
+            sportsman.IncreaseExerciseIntensity();
+            sportsman.IncreaseExerciseIntensity();
+        }
+    }
+}
