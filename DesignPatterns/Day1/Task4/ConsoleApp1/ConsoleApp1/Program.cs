84c08bc21e2139daa6570794b9fd64c495afaff8
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-25 09:39:31 +0200

    Added task 4

---
+++ DesignPatterns/Day1/Task4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserRepository repo = new UserRepository(new MySqlDb(new MySqlConnection()));
+            Console.WriteLine($"Users count {repo.GetUsers().Count}");
+            Console.ReadLine();
+
+            //Dependency Inversion principle - modules in program should depend on abstractions not otherwise, so we are creating -
+            // interfaces to not violate this principle
+        }
+    }
+}
