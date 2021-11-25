a9259199eafa5a3569645c0400bad205b3ce25f0
KateFedak <katefedakk@gmail.com>
2021-11-25 15:11:10 +0200

    Template Method

---
+++ DesignPatterns/Day4/BehavioralPatterns/TemplateMethod/Furniture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal class Furniture : Purchase
+    {
+        public override void GetOut()
+        {
+            Console.WriteLine("Get out of home");
+        }
+
+        public override void GoToThe()
+        {
+            Console.WriteLine("Go to the store");
+        }
+
+        public override void Pay()
+        {
+            Console.WriteLine("Write a check");
+        }
+
+        public override void OrderDeliveryService()
+        {
+            Console.WriteLine("Order delivery service");
+        }
+    }
+}
--- DesignPatterns/Day4/BehavioralPatterns/TemplateMethod/Program.cs
+++ DesignPatterns/Day4/BehavioralPatterns/TemplateMethod/Program.cs
@@ -6,7 +6,20 @@ namespace TemplateMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("********************************");
+            Console.WriteLine("Products:");
+            var products = new Products();
+            products.Shopping();
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("Gift:");
+            var gift = new Gift();
+            gift.Shopping();
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("Furniture:");
+            var furniture = new Furniture();
+            furniture.Shopping();
         }
     }
 }
