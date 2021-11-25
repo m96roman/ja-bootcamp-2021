fd28b80b86d39e93917b0ea60b47345b96674228
Martin Roshko <martin.roshko1@gmail.com>
2021-11-25 15:22:53 +0200

    Flyweight

---
+++ DesignPatterns/Day3/Flyweight/Flyweight/FlyweightFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public class FlyweightFactory
+    {
+        private List<Tuple<Flyweight, string>> flyweights = new List<Tuple<Flyweight, string>>();
+
+        public FlyweightFactory(Build[] street)
+        {
+            foreach (var build in street)
+            {
+                flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(build), build._Adress));
+            }
+        }
+
+        public Flyweight GetFlyweight(Build build)
+        {
+            string key = build._Adress;
+
+            if (flyweights.Where(t => t.Item2 == key).Count() == 0)
+            {
+                Console.WriteLine("FlyweightFactory: Can't find a flyweight, creating new one.");
+                this.flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(build), key));
+            }
+            else
+            {
+                Console.WriteLine("FlyweightFactory: Reusing existing flyweight.");
+            }
+            return this.flyweights.Where(t => t.Item2 == key).FirstOrDefault().Item1;
+        }
+
+        public void listFlyweights()
+        {
+            var count = flyweights.Count;
+            Console.WriteLine($"\nFlyweightFactory: I have {count} flyweights:");
+            foreach (var flyweight in flyweights)
+            {
+                Console.WriteLine(flyweight.Item2);
+            }
+        }
+    }
+}
