50d55ef9b558a37646a0da65efd512e0a05c5d20
Viktoriia Dedenok <viktoriia.dedenok@justanswer.com>
2021-11-25 13:48:50 +0200

    Design Patterns Dedenok Day3

---
+++ DesignPatternDay3/Facade/Facade/Compiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Compiler
+    {
+        public void Validate()
+        {
+            Console.WriteLine("I am doing validation.");
+        }
+
+        public void Compile()
+        {
+            Console.WriteLine("I am doing compilation.");
+        }
+    }
+}
