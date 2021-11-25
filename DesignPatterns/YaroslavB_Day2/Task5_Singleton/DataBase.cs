49155d4e153fca2356587a8bc567421e36127d25
Yaroslav-B <sync2galaxy@gmail.com>
2021-11-25 20:03:23 +0200

    Task5 - Singleton

---
+++ DesignPatterns/YaroslavB_Day2/Task5_Singleton/DataBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task5_Singleton
+{
+    class DataBase
+    {
+        private static DataBase _instance;
+        public static DataBase GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new DataBase();
+            }
+
+            return _instance;
+        }
+
+        private DataBase() { }
+
+        public void SqlQuery(string query)
+        {
+            Console.WriteLine($"Query: \'{query}\'");
+        }
+    }
+}
---
+++ DesignPatterns/YaroslavB_Day2/Task5_Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task5_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending SQL query...");
+
+            DataBase db = DataBase.GetInstance();
+            db.SqlQuery("select * from users");
+
+            Console.WriteLine("\n\nPress \'Enter\' to exit...");
+            Console.ReadLine();
+        }
+    }
+}
