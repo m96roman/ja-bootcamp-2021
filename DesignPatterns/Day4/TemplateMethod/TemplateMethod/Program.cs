7388af447b02dfb32874dcb51fb1a47c3edaafd9
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-25 16:09:59 +0200

    Template added

---
+++ DesignPatterns/Day4/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingTemplate shopping = new Groceries();
+            shopping.ShoppingTemplateSimulation();
+        }
+    }
+}
---
+++ DesignPatterns/Day4/TemplateMethod/TemplateMethod/ShoppingTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    abstract class ShoppingTemplate
+    {
+        public void ShoppingTemplateSimulation()
+        {
+            this.PrepareForShopping();
+            this.ChoosePlace();
+            this.TakeCart();
+            this.ChooseWhatYouNeed();
+            this.GoToCheckOut();
+            this.PayForItems();
+            this.CallForDelivery();
+            this.TakeHome();
+        }
+
+        public abstract void PrepareForShopping();
+        public abstract void ChoosePlace();
+        public virtual void ScoutDepartments() { }
+        public virtual void TakeCart() { }
+        public abstract void ChooseWhatYouNeed();
+        public virtual void GoToCheckOut() { }
+        public abstract void PayForItems();
+        public virtual void CallForDelivery() { }
+        public abstract void TakeHome();
+    }
+}
