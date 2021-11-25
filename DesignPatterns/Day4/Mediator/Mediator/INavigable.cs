afe359b3251a078ab8fa1713bdb799fc203b2cb1
Dmytro Ivanyshyn <dmytro.ivanyshyn@justanswer.com>
2021-11-25 16:22:40 +0200

    Mediator done

---
+++ DesignPatterns/Day4/Mediator/Mediator/INavigable.cs
@@ -0,0 +1,10 @@
+﻿namespace Mediator
+{
+    public interface INavigable
+    {
+        string Name { get; set; }
+
+        void SetMediator(IDispatcher dispetcher);
+        void Maneuver(string message);
+    }
+}
---
+++ DesignPatterns/Day4/Mediator/Mediator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ship ship1 = new Ship
+            {
+                Name = "Lviv"
+            };
+
+            Ship ship2 = new Ship
+            {
+                Name = "Uzhorod"
+            };
+
+            Ship ship3 = new Ship
+            {
+                Name = "Kyiv"
+            };
+
+
+            UkrainianDispatcher ukrainianDispatcher = new UkrainianDispatcher(new List<INavigable> {
+               ship1,
+               ship2,
+               ship3
+            });
+
+            ship1.Dispatcher.Notify(ship1, "Kyiv", "move out dumbass");
+            ship3.Dispatcher.Notify(ship2, "Lviv", "move right please");
+            ship3.Dispatcher.Notify(ship3, "Uzhorod", "move left please");
+        }
+    }
+}
