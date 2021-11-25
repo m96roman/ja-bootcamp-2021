1efecd6d8e48f23c6ffd9a1ae88a836d2350dcd5
Martin Roshko <martin.roshko1@gmail.com>
2021-11-25 16:27:08 +0200

    Facade

---
+++ DesignPatterns/Day3/Facade/Facade/Collector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Collector
+    {
+        public void CreateLibrary()
+        {
+            Console.WriteLine("Library was created");
+        }
+
+        public void CopyIntoFolder() 
+        {
+            Console.WriteLine("File copy to folder");
+        }
+    }
+}
---
+++ DesignPatterns/Day3/Facade/Facade/TextEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class TextEditor
+    {
+        public void Save() 
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+}
