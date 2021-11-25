68813a64e3b1810ac68492f3e4f942891fe94401
Martin Roshko <martin.roshko1@gmail.com>
2021-11-25 14:11:47 +0200

    Adapter

---
+++ DesignPatterns/Day3/Adapter/Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Adapter : IMessage 
+    {
+        private IOldFashionMessage _OldFashionMessage { get; set; }
+
+        public Adapter(IOldFashionMessage oldFashionMessage) 
+        {
+            this._OldFashionMessage = oldFashionMessage;
+        }
+
+        public void Send(string text)
+        {
+            _OldFashionMessage.KickThebird(text);
+        }
+    }
+}
