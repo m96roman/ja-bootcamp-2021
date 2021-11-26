cd1288cb444420683e33dd3090c7a4cde4035373
Martin Roshko <martin.roshko1@gmail.com>
2021-11-26 15:35:18 +0200

    Mediator

---
+++ DesignPatterns/Day4/Mediator/Mediator/Ships/KievShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Ships
+{
+    class KievShip : Ship
+    {
+        public KievShip(IMediator mediator) : base(mediator)
+        {
+            this.Name = "KievShip";
+        }
+    }
+}
---
+++ DesignPatterns/Day4/Mediator/Mediator/Ships/UzhgorodShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Ships
+{
+    class UzhgorodShip : Ship
+    {
+        public UzhgorodShip(IMediator mediator) : base(mediator)  
+        {
+            this.Name = "UzhgorodShip";
+        }
+    }
+}
