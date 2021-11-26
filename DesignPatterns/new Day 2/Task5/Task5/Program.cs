9bea344ed113b04c21bd30899fc6b77d3f22987f
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-26 16:07:50 +0200

    Task 5

---
+++ DesignPatterns/new Day 2/Task5/Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database() { Name = "Nestor", LastName = "Pylat" };
+            database.getInstance();
+            database.getInstance();
+            database.getInstance();
+            database.getInstance();
+        }
+    }
+}
