fd74b0b1cbc3f36035f838c42d60265f1e936f02
KateFedak <katefedakk@gmail.com>
2021-11-26 11:02:28 +0200

    improve Mediator

--- DesignPatterns/Day4/BehavioralPatterns/Mediator/Program.cs
+++ DesignPatterns/Day4/BehavioralPatterns/Mediator/Program.cs
@@ -8,22 +8,13 @@ namespace Mediator
         {
 
             Desptcher sDesptcher = new Desptcher();
-            var lvivShip = new LvivShip(sDesptcher);
-            var kievShip = new KievShip(sDesptcher);
-            var uzhorodShip = new UzhorodShip(sDesptcher);
+            sDesptcher.Ships.Add(new LvivShip(sDesptcher));
+            sDesptcher.Ships.Add(new KievShip(sDesptcher));
+            sDesptcher.Ships.Add(new UzhorodShip(sDesptcher));
 
-            sDesptcher.KievShip = kievShip;
-            sDesptcher.LvivShip = lvivShip;
-            sDesptcher.UzhhorodShip = uzhorodShip;
-            sDesptcher.Ships.Add(lvivShip);
-            sDesptcher.Ships.Add(kievShip);
-            sDesptcher.Ships.Add(uzhorodShip);
-
-            kievShip.Send("Hi my friend",lvivShip.Name);
-            lvivShip.Send("Heloooooooo",kievShip.Name);
-            uzhorodShip.Send("I am here", kievShip.Name);
-            kievShip.Send("I see",uzhorodShip.Name);
-
+            sDesptcher.Ships.Find(s => s.Name == "Kiev").Send("Hello Lviv!","Lviv");
+            sDesptcher.Ships.Find(s => s.Name == "Lviv").Send("OH! Hi Kiev!", "Kiev");
+            sDesptcher.Ships.Find(s => s.Name == "Uzhorod").Send("I see you!", "Lviv");
         }
     }
 }
