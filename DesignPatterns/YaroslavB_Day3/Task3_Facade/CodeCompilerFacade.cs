c265008e11794a548bc72f1f80e6452c5a644bd9
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-11-26 11:19:22 +0200

    Task3_Facade

---
+++ DesignPatterns/YaroslavB_Day3/Task3_Facade/CodeCompilerFacade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task3_Facade
+{
+    class CodeCompilerFacade
+    {
+        private const string _tempWorkDirectory = "C:\\Temp";
+
+        public void CreateFinalPackage(string pathToCode, string pathtoOutputFolder)
+        {
+            var codeCompiler = new CodeCompiler();
+            bool result = codeCompiler.ValidateSourceCode(pathToCode);
+
+            if(!result)
+            {
+                Console.WriteLine($"Exeption: Code by path \'{pathToCode}\' failed validation!");
+                return;
+            }
+
+            codeCompiler.CompileCodeToBinaryFiles(pathToCode, _tempWorkDirectory);
+
+            var builder = new Builder();
+            builder.BuildLibraries(_tempWorkDirectory, pathtoOutputFolder);
+        }
+    }
+}
---
+++ DesignPatterns/YaroslavB_Day3/Task3_Facade/CodeCompilerTool/Builder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task3_Facade
+{
+    class Builder
+    {
+        public void BuildLibraries(string workDirectoryPath, string outputFolder)
+        {
+            Console.WriteLine($"Has been built set of libraries and stored to output folder: \'{outputFolder}\'");
+        }
+    }
+}
