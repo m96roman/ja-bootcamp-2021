cc37c5d10fa61348f5f02e16040300d4fd6fd8e0
tennesseee <51917105+tennesseee@users.noreply.github.com>
2021-11-26 10:34:13 +0200

    Added strategy

---
+++ DesignPatterns/Day4/ConsoleApp1/Strategy/Strategy/DecoratingMan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class DecoratingMan
+    {
+        private IDecoration _decoration;
+
+        public DecoratingMan(IDecoration decoration)
+        {
+            _decoration = decoration;
+        }
+
+        public void DecorateWalls()
+        {
+            _decoration.DecorateWalls();
+        }
+    }
+}
