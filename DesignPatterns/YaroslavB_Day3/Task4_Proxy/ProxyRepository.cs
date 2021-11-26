7a960c347cd356650956b1e83cc1505f240d0fa4
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-11-26 14:50:55 +0200

    Task4_Proxy

---
+++ DesignPatterns/YaroslavB_Day3/Task4_Proxy/ProxyRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4_Proxy
+{
+    class ProxyRepository : IUserRepository
+    {
+        private VodafoneUserRepository _realRepository;
+        private List<string> _cachedUsers;
+        private readonly int _cacheLiveTimeSec = 60;
+        private DateTime _lastTimeRealRequest;
+
+        public ProxyRepository(VodafoneUserRepository realRepository)
+        {
+            _realRepository = realRepository;
+            _cachedUsers = RealRequest();
+        }
+
+        public List<string> GetUserList()
+        {
+            if(IsChachActual())
+            {
+                Console.WriteLine("DebugMessage: got users from cache.");
+                return _cachedUsers;
+            }
+
+            _cachedUsers = RealRequest();
+
+            return _cachedUsers;
+        }
+
+        private bool IsChachActual()
+        {
+            var currentTime = DateTime.Now;
+            var interval = (currentTime - _lastTimeRealRequest).TotalSeconds;
+
+            Console.WriteLine($"DebugMessage: time since last external request: {interval}");
+
+            if (interval < _cacheLiveTimeSec)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private List<string> RealRequest()
+        {        
+            var users = _realRepository.GetUserList();
+            _lastTimeRealRequest = DateTime.Now;
+            Console.WriteLine("DebugMessage: done real reaquest to DB.");
+
+            return users;
+        }
+    }
+}
---
+++ DesignPatterns/YaroslavB_Day3/Task4_Proxy/VodafoneUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Task4_Proxy
+{
+    public class VodafoneUserRepository : IUserRepository
+    {
+        public List<string> GetUserList()
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            return new List<string> { "John", "Rob", "Monica" };
+        }
+    }
+}
