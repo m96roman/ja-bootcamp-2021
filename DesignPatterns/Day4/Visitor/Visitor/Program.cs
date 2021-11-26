07b5a4821453b23159d1300c65607cf60d90fed0
Martin Roshko <martin.roshko1@gmail.com>
2021-11-26 11:43:25 +0200

    Visitor

---
+++ DesignPatterns/Day4/Visitor/Visitor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Visitor.Serializers;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonSerializer = new JsonSerializerVisitor();
+            var notJsonSerializer = new NotJsonSerializerVisitor();
+
+            var car = new Car()
+            {
+                Brand = "Audi",
+                Model = "Q7",
+                Color = "Gray",
+                Date = new Date() { Day = 04, Mounth = 8, Year = 2020 },
+                SerialNumber = 00020402832,
+            };
+
+            var driver = new Driver()
+            {
+                Name = "Martin",
+                Surname = "Roshko",
+                DateOfBirthday = new Date() { Day = 15, Mounth = 12, Year = 2000 },
+                DriverLicenseNumber = 328193288211,
+                DateIssueDriverLicense = new Date() { Day = 22, Mounth = 02, Year = 2018 },
+            };
+
+            car.InfoSerialize(jsonSerializer);
+            car.InfoSerialize(notJsonSerializer);
+
+            driver.InfoSerialize(jsonSerializer);
+            driver.InfoSerialize(notJsonSerializer);
+
+        }
+    }
+}
---
+++ DesignPatterns/Day4/Visitor/Visitor/Serializers/JsonSerializerVisitor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor.Serializers
+{
+    class JsonSerializerVisitor : IVisitor
+    {
+        public void VCar(Car car)
+        {
+            Console.WriteLine($"{nameof(car)}");
+            ShowInfo(new List<object>()
+            { 
+                car.Brand,
+                car.Model,                
+                car.Date,
+                car.Color,
+                car.SerialNumber 
+            });
+        }
+
+        public void VDriver(Driver driver)
+        {
+            Console.WriteLine($"{nameof(driver)}");
+            ShowInfo(new List<object>()
+            {
+                driver.Name,
+                driver.Surname,
+                driver.DateOfBirthday,
+                driver.DriverLicenseNumber,
+                driver.DateIssueDriverLicense
+            });
+        }
+
+        private void ShowInfo(List<object> list) 
+        {   
+            foreach (var iteam in list) 
+            {
+                if (iteam.GetType() == typeof(Date))
+                {
+                    var date = (Date)iteam;
+                    Console.Write('{'+ $"{nameof(date)}:\"{date.GetDate()}\"" + '}' + ';');
+                }else
+                {
+                    Console.Write('{' + $"{nameof(iteam)}:\"{iteam}\"" + '}' + ';');
+                }                
+            }
+            Console.WriteLine();
+        }
+    }
+}
