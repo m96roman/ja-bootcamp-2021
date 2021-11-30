8de09c3078635730d466cbe063fb201cd64146c0
viktoriiadedenok <56121643+viktoriiadedenok@users.noreply.github.com>
2021-11-30 11:45:14 +0200

    Commiting an untracked files

---
+++ FullName/FullName/Controllers/BindeNamesController.cs
@@ -0,0 +1,33 @@
+﻿using FullName.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NUnit.Framework.Internal.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullName.Controllers
+{
+    public class BindeNamesController : Controller
+    {
+        private static List<BindedName> bindedNames = new List<BindedName>();
+        
+        public IActionResult SetNamesToBind()
+        {
+            return View();
+        }
+  
+        [HttpPost]
+        public IActionResult SetNamesToBind(BindedName name)
+        {
+            bindedNames.Add(name);
+            return RedirectToAction("ShowBindedNames");
+        }
+
+        public IActionResult ShowBindedNames()
+        {
+            return View(bindedNames);
+        }
+    }
+}
---
+++ FullName/FullName/Infrastructure/CustomNameModelBinder.cs
@@ -0,0 +1,46 @@
+﻿using FullName.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FullName.Infrastructure
+{
+    public class CustomNameModelBinder: IModelBinder
+    {
+        private readonly IModelBinder fallbackBinder;
+
+        public CustomNameModelBinder(IModelBinder fallbackBinder)
+        {
+            this.fallbackBinder = fallbackBinder;
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {         
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+      
+            var firstName = bindingContext.ValueProvider.GetValue("FirstName");
+            var lastName = bindingContext.ValueProvider.GetValue("LastName");
+
+            if (firstName == ValueProviderResult.None || lastName == ValueProviderResult.None)
+                return fallbackBinder.BindModelAsync(bindingContext);
+
+            Regex reg = new Regex("[^a-zA-Z']");
+
+            string first = reg.Replace(firstName.FirstValue, string.Empty);
+            string last = reg.Replace(firstName.FirstValue, string.Empty);
+
+            var result = new BindedName() { BindedFirstName = first, BindedLastName = last };
+        
+            bindingContext.Result = ModelBindingResult.Success(result);
+            return Task.CompletedTask;
+        }
+    }
+}
+
