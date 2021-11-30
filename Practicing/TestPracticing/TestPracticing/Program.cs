ba464085e0b913b17853823bcca527a752fdf8d6
viktoriiadedenok <56121643+viktoriiadedenok@users.noreply.github.com>
2021-11-30 17:33:37 +0200

    add

---
+++ Practicing/TestPracticing/TestPracticing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPracticing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
